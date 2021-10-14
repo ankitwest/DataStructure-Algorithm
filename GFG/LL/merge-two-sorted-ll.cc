200862657fc926ff44a8a330c91f5b5c1723170f
Ankit Kumar <anshkrrr@gmail.com>
2021-10-14 14:07:21 +0530

    Update merge-two-sorted-ll.cc

--- GFG/LL/merge-two-sorted-ll.cc
+++ GFG/LL/merge-two-sorted-ll.cc
@@ -1,3 +1,6 @@
+Recursive
+
+
 Node* sortedMerge(Node* head1, Node* head2)  
 {  
     if(head1==NULL) return head2;
@@ -13,3 +16,40 @@ Node* sortedMerge(Node* head1, Node* head2)
     }
     return head;
 } 
+
+
+Iterative
+
+
+Node* sortedMerge(Node* head1, Node* head2)  {
+    if(head1==NULL) return head2;
+    if(head2==NULL) return head1;
+    
+    Node* curr;
+    if(head1->data<head2->data){
+        curr=head1; 
+        head1=head1->next;
+    }else{
+        curr=head2; 
+        head2=head2->next;
+    }
+    
+    Node* head = curr;
+    while(head1 and head2){
+        if(head1->data<head2->data){
+            curr->next=head1;
+            head1=head1->next;
+            curr=curr->next;
+        }else{
+            curr->next=head2; 
+            head2=head2->next;
+            curr=curr->next;
+        }
+        
+    }
+    if(head2) curr->next=head2;
+    else curr->next=head1;
+    
+    return head;
+    
+}
