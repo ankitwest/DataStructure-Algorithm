cb56bd15b9f154728dbb39fbf36232832c2b2834
Ankit Kumar <anshkrrr@gmail.com>
2021-10-24 12:32:51 +0530

    Create cycle-in-undirected-graph.cc

---
+++ GFG/Graph/cycle-in-undirected-graph.cc
@@ -0,0 +1,66 @@
+class Solution {
+  public:
+  vector<bool> vis;
+  bool ans ;
+    void dfs(int node,vector<int> adj[],int parent){
+        vis[node]=1;
+        for(auto neigh: adj[node]){
+            if(neigh==parent)  continue;  //YEH TOH BAAP HAI... OH SHIT NOT GOOD
+          
+            if(vis[neigh]){
+                ans = true; return ;
+            }else if(!vis[neigh]){
+                dfs(neigh,adj,node);
+            }
+        }
+    }
+    bool isCycle(int V, vector<int> adj[]) {
+        vis.resize(V,false);
+        ans = false;
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                 dfs(i,adj,-1);
+            }
+        }
+        return ans;
+    }
+};
+
+
+Expected Time Complexity: O(V + E)
+Expected Space Complexity: O(V)
+  
+  
+  
+  
+  class Solution {
+  public:
+  vector<bool> vis;
+  bool ans ;
+    bool dfs(int node,vector<int> adj[],int parent){
+        vis[node]=1;
+        for(auto neigh: adj[node]){
+            if(neigh==parent)  continue;  //YEH TOH BAAP HAI... OH SHIT NOT GOOD
+           
+            if(vis[neigh]){
+                return true;
+            }else if(!vis[neigh]){
+                if(dfs(neigh,adj,node)) 
+                    return true;
+            }
+        }
+        return false;
+        
+    }
+    bool isCycle(int V, vector<int> adj[]) {
+        vis.resize(V,false);
+        ans = false;
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                if(dfs(i,adj,-1)) 
+                    return true;
+            }
+        }
+        return false;
+    }
+};
