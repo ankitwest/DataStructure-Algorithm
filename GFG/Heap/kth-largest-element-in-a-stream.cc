2ca6bdf0fb7638763fac85630051c23602362802
Ankit Kumar <anshkrrr@gmail.com>
2021-10-26 14:20:49 +0530

    Create kth-largest-element-in-a-stream.cc

---
+++ GFG/Heap/kth-largest-element-in-a-stream.cc
@@ -0,0 +1,25 @@
+class Solution{
+public:
+    vector<int> kthLargest(int k, int arr[], int n) {
+        priority_queue<int,vector<int>,greater<int>>minpq;
+        // sort(arr,arr+n);
+        vector<int> ans(n);
+        for(int i=0;i<n;i++){
+            if(minpq.size()<k)
+                minpq.push(arr[i]);
+            else{
+                if(arr[i]>minpq.top())
+                { minpq.pop();  minpq.push(arr[i]); }
+            }
+            if(minpq.size()<k)
+                ans[i] = -1;
+            else
+               ans[i] = minpq.top();
+        }
+          return ans;
+    }
+};
+
+
+Expected Time Complexity: O(nlogk)
+Expected Auxiliary Space: O(n)
