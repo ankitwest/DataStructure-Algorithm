729b3ec996856ba82372e0ccfd709268d084585e
Ankit Kumar <anshkrrr@gmail.com>
2021-10-27 07:48:07 +0530

    Update josephus-problem.cc

--- GFG/Recursion/josephus-problem.cc
+++ GFG/Recursion/josephus-problem.cc
@@ -1,4 +1,28 @@
 class Solution{
+ public:
+    int josephus(int n, int k)
+    {
+      if(n==1) return 1;
+      else return (josephus(n-1,k)+k-1)%n+1;
+    }
+];
+
+
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N).
+
+
+
+
+
+
+
+
+
+
+
+class Solution{
       public:
     int josephus(int n, int k)
     {
