785ecec0b0bbe1de045bd06943754a032a72737b
Ankit Kumar <anshkrrr@gmail.com>
2021-10-30 10:17:14 +0530

    Update height.cc

--- GFG/BinaryTree/height.cc
+++ GFG/BinaryTree/height.cc
@@ -3,6 +3,17 @@ class Solution{
     //Function to find the height of a binary tree.
     int height(struct Node* root){
        if(!root) return 0;
+       return  1 + max(height(root->left),height(root->right));
+    }
+};
+
+
+
+class Solution{
+    public:
+    //Function to find the height of a binary tree.
+    int height(struct Node* root){
+       if(!root) return 0;
        if(!root->left and !root->right) return 1;
        
        return  1 + max(height(root->left),height(root->right));
