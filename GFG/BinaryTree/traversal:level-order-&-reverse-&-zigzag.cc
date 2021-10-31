4639cdc9b5481e81c40f162119bef9144f6855be
Ankit Kumar <anshkrrr@gmail.com>
2021-10-31 08:16:34 +0530

    Update and rename level-order-&-reverse-level-order.cc to traversal:level-order-&-reverse-&-zigzag.cc

--- GFG/BinaryTree/traversal:level-order-&-reverse-&-zigzag.cc
+++ GFG/BinaryTree/traversal:level-order-&-reverse-&-zigzag.cc
@@ -1,8 +1,8 @@
 
+//level order traversal
 class Solution
 {
     public:
-    //Function to return the level order traversal of a tree.
     vector<int> levelOrder(Node* node)
     {
       vector<int> v;
@@ -30,7 +30,7 @@ Expected Time Complexity: O(N)
 
     
     
-    
+//reverse level order traversal    
 Using Vector    
 vector<int> reverseLevelOrder(Node *node)
 {
@@ -88,3 +88,45 @@ vector<int> reverseLevelOrder(Node *node)
 }
 
 Expected Time Complexity: O(N)
+    
+    
+    
+    
+    
+  //zigzag level order traversal    
+    
+ class Solution{
+    public:
+    //Function to store the zig zag order traversal of tree in a list.
+    vector <int> zigZagTraversal(Node* root)
+    {
+    	vector<int> ans;
+    	if(!root) return ans;
+    	
+    	queue<Node*> q;
+    	q.push(root);
+    	int k=0;
+    	while(!q.empty()){
+    	    int n = q.size();
+    	    vector<int> v;
+    	    for(int i=0;i<n;i++){
+    	        Node* curr = q.front();q.pop();
+    	        v.push_back(curr->data);
+    	        if(curr->left)
+    	            q.push(curr->left);
+    	       if(curr->right)
+    	            q.push(curr->right);
+    	        
+    	    }
+    	    if(k%2==1)
+    	        reverse(v.begin(),v.end());
+    	    for(auto x:v)
+    	        ans.push_back(x);
+    	    k++;
+    	}
+    	return ans;
+    }
+};
+
+
+Expected Time Complexity: O(N).
