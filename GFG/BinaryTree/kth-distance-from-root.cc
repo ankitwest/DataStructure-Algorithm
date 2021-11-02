f3b8c9c0ff3505d32e413e7b4c9d02de1b7f31c5
Ankit Kumar <anshkrrr@gmail.com>
2021-11-02 10:00:53 +0530

    Update kth-distance-from-root.cc

--- GFG/BinaryTree/kth-distance-from-root.cc
+++ GFG/BinaryTree/kth-distance-from-root.cc
@@ -2,11 +2,11 @@ class Solution{
   public:
   void helper(Node* root,int k, vector<int> &v){
     if(!root) return ;
-    if(k==0)
-        v.push_back(root->data);
         
     helper(root->left,k-1,v);
     helper(root->right,k-1,v);
+    if(k==0)
+      v.push_back(root->data);
     
 }
 vector<int> Kdistance(struct Node *root, int k)
