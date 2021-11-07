49885e3628f263d99da877a1751f65e3c3610541
Ankit Kumar <anshkrrr@gmail.com>
2021-11-07 22:59:40 +0530

    Update 2-identical-trees-&-mirror-trees-&-symmetric-trees.cc

--- GFG/BinaryTree/2-identical-trees-&-mirror-trees-&-symmetric-trees.cc
+++ GFG/BinaryTree/2-identical-trees-&-mirror-trees-&-symmetric-trees.cc
@@ -15,6 +15,40 @@ class Solution
     }
 };
 
+// Check for Subtree
+class Solution
+{
+  public:
+    bool identical(Node* r1,Node* r2){
+        if(!r1 and !r2) return true;
+        if(!r1 or !r2) return false;
+        
+        if(r1->data!=r2->data) return false;
+        return (r1->data==r2->data) and identical(r1->left,r2->left) and identical(r1->right,r2->right);
+    }
+    bool isSubTree(Node* T, Node* S) 
+    {
+       queue<Node*> q;
+       q.push(T);
+       while(!q.empty()){
+           auto curr = q.front();q.pop();
+           if(curr->data==S->data) {
+               if(identical(curr,S))
+                return true;
+           }
+           if(curr->left) q.push(curr->left);
+           if(curr->right) q.push(curr->right);
+       }
+       return false;
+    }
+};
+Input:
+T:      1          S:   3
+      /   \            /
+     2     3          4
+   /  \    /
+  N    N  4
+Output: 1 
 
 
 // Invert a tree
