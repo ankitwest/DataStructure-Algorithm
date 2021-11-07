82511e72c444c5bca0904581c3e5a0693789b035
Ankit Kumar <anshkrrr@gmail.com>
2021-11-07 19:28:32 +0530

    Update sudoku-solver.cc

--- GFG/Backtracking/sudoku-solver.cc
+++ GFG/Backtracking/sudoku-solver.cc
@@ -1,6 +1,88 @@
 
+// GFG
+class Solution{
+ public:
+    //Function to find a solved Sudoku. 
+    bool valid(int row,int col,int grid[N][N],int val){
+        for(int i=0;i<9;i++){
+            if(row==i) continue;
+            if(grid[i][col]==val) return false;
+        }
+        for(int i=0;i<9;i++){
+            if(col==i) continue;
+            if(grid[row][i]==val) return false;
+        }
+        
+        int start_row=row/3*3;
+        int start_col=col/3*3;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(row==i+start_row and col==j+start_col) continue;
+                 if(grid[i+start_row][j+start_col]==val) return false;
+            }
+        }
+        return true;
+        
+    }
+//         bool sudoku(int i,int j,int grid[N][N]){
+//          for(int i=0;i<N;i++){
+//             for(int j=0;j<N;j++){
+//                 if(grid[i][j]==0){
+//                     for(int choice=1;choice<=9;choice++){
+//                         if(valid(i,j,grid,choice)){
+//                             grid[i][j]=choice;
+//                             if(sudoku(i,j,grid)) return true;
+//                             grid[i][j]=0;
+//                         }
+//                     }//for loop
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true; 
+//     }
+    
+    bool sudoku(int i,int j,int grid[N][N]){
+        if(i==N) return true;
+            
+        int new_i=0,new_j=0;
+        if(j==N-1) new_i=i+1,new_j=0;
+        else new_i=i,new_j=j+1;
+        
+        //Already filled
+        if(grid[i][j]!=0){
+            return sudoku(new_i,new_j,grid);
+        }
+        else{  // Empty boxes
+            for(int k=1;k<=9;k++){
+                if(valid(i,j,grid,k)){
+                  grid[i][j]=k;
+                  if(sudoku(new_i,new_j,grid)) return true;
+                  grid[i][j]=0;
+                }
+            }
+            return false;
+        }
+        return true;
+    }
+    bool SolveSudoku(int grid[N][N])  
+    { 
+        return sudoku(0,0,grid);
+    }
+    void printGrid (int grid[N][N]) 
+    {
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                cout<<grid[i][j]<<" ";
+            }
+//             cout<<endl;
+        }
+    }
+};
+Expected Time Complexity: O(9^(N*N)).
 
-// LeetCode
+
+// LeetCode  copied
 
 class Solution {
 public:
@@ -44,6 +126,11 @@ public:
         return true;
     }
 };
+
+
+
+
+
 
 
 
