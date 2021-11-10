05a195e435d8c2f2a51695ce114075f68caed7ae
Ankit Kumar <anshkrrr@gmail.com>
2021-11-10 15:23:53 +0530

    Update cycle-in-undirected-graph.cc

--- GFG/Graph/cycle-in-undirected-graph.cc
+++ GFG/Graph/cycle-in-undirected-graph.cc
@@ -2,32 +2,26 @@
 //Cycle in UG graph
 
 class Solution {
-  public:
-  vector<bool> vis;
-  bool ans ;
-    bool dfs(int node,vector<int> adj[],int parent){
+public:
+    bool dfs(int node,int parent,vector<int> adj[],vector<bool> &vis){
         vis[node]=1;
-        for(auto neigh: adj[node]){
-            if(neigh==parent)  continue;  //YEH TOH BAAP HAI... OH SHIT NOT GOOD
-           
-            if(vis[neigh]){
-                return true;
-            }else if(!vis[neigh]){
-                if(dfs(neigh,adj,node)) 
-                    return true;
+        for(auto child:adj[node]){
+            if(!vis[child]){
+                if(dfs(child,node,adj,vis)) return true;
+            }else if(vis[child]){
+                if(child==parent) continue;
+                else if(child!=parent) return true;
             }
         }
         return false;
-        
     }
+    
     bool isCycle(int V, vector<int> adj[]) {
-        vis.resize(V,false);
-        ans = false;
+        vector<bool> vis(V,0);
         for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(dfs(i,adj,-1)) 
+            if(!vis[i])
+                if(dfs(i,-1,adj,vis))
                     return true;
-            }
         }
         return false;
     }
@@ -35,40 +29,40 @@ class Solution {
 Expected Time Complexity: O(V + E)
 Expected Space Complexity: O(V)
 
-  
-  
-// class Solution {
-//   public:
-//   vector<bool> vis;
-//   bool ans ;
-//     void dfs(int node,vector<int> adj[],int parent){
-//         vis[node]=1;
-//         for(auto neigh: adj[node]){
-//             if(neigh==parent)  continue;  //YEH TOH BAAP HAI... OH SHIT NOT GOOD
-          
-//             if(vis[neigh]){
-//                 ans = true; return ;
-//             }else if(!vis[neigh]){
-//                 dfs(neigh,adj,node);
-//             }
-//         }
-//     }
-//     bool isCycle(int V, vector<int> adj[]) {
-//         vis.resize(V,false);
-//         ans = false;
-//         for(int i=0;i<V;i++){
-//             if(!vis[i]){
-//                  dfs(i,adj,-1);
-//             }
-//         }
-//         return ans;
-//     }
-// };
 
 
 
+  //Cycle in DG graph
   
+  class Solution {
+public:
+    bool dfs(int node,int parent,vector<int> adj[],vector<bool> &vis,vector<bool> &anc){
+        vis[node]=1;
+        anc[node]=1;
+        for(auto child:adj[node]){
+            if(!vis[child]){
+                if(dfs(child,node,adj,vis,anc)) return true;
+            }else if(anc[child]==1) return true;
+        }
+         anc[node]=0;
+        return false;
+    }
+    
+   bool isCyclic(int V, vector<int> adj[]) {
+        vector<bool> vis(V,0);
+        vector<bool> anc(V,0);
+        for(int i=0;i<V;i++){
+            if(!vis[i])
+                if(dfs(i,-1,adj,vis,anc))
+                    return true;
+        }
+        return false;
+    }
+};
+Expected Time Complexity: O(V + E)
+Expected Space Complexity: O(V)
   
-  
+  1->2->3  cycle yes
+  1->2->3->4  1->4 cycle no
   
  
