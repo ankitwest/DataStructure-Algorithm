e50ff1b8f00ddfb9bbd3260cec99e2acc805b71f
Ankit Kumar <anshkrrr@gmail.com>
2021-11-11 22:24:20 +0530

    Update dijkstras-algo.cc

--- GFG/Graph/dijkstras-algo.cc
+++ GFG/Graph/dijkstras-algo.cc
@@ -2,26 +2,26 @@ class Solution{
   	public:
 	//Function to find the shortest distance of all the vertices
     //from the source vertex S.
-    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int src)
     {
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         vector<int> dist(V,INT_MAX);
         
-        dist[S]=0;
-        pq.push({0,S});
+        dist[src]=0;
+        pq.push({0,src});
         while(!pq.empty()){
-            // int dist = pq.top().first;
-            int prev = pq.top().second;
+            // int dis = pq.top().first;
+            int u = pq.top().second;
             pq.pop();
             
-            for(auto neigh : adj[prev]){
-                    int next=neigh[0];
-                    int wt=neigh[1];
+            for(auto neigh : adj[u]){
+                    int v = neigh[0];
+                    int wt = neigh[1];
                 
-                if(dist[prev]+wt<dist[next])
+                if(dist[u]+wt<dist[v])
                 {
-                    dist[next]=dist[prev]+wt;
-                    pq.push({dist[next],next});
+                    dist[v]=dist[u]+wt;
+                    pq.push({dist[v],v});
                 }
             }
         }
@@ -53,6 +53,35 @@ Expected Time Complexity: O(V^2).
 	for(int i = 1 ; i<=n ; i++)	cout << distTo[i] << " ";
 	cout << "\n";
 	
-	return 0;
-	
+	return 0;	
 }
+
+
+
+// using set
+
+//     vector <int> dijkstra(int N, vector<vector<int>> adj[], int src){
+//         set<pair<int, int>> s;
+//     	vector<int> dist(N, INT_MAX);
+    
+//     	s.insert({0, src});
+//     	dist[src] = 0; 
+    
+//     	while(!s.empty()){
+//     		auto itr = *s.begin();
+//     		// int dis = itr.first;
+//     		int u = itr.second;
+//     		s.erase(itr);
+    
+//     		for(auto neigh:adj[u]){
+//     			int v = neigh[0];
+//     			int wt = neigh[1];
+    
+//     			if(dist[u] + wt < dist[v]){
+//     				dist[v] = dist[u] + wt;
+//     				s.insert({dist[v], v});
+//     			}
+//     		}
+//     	}
+//     	return dist;
+//     }
