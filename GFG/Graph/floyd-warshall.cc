9088ba51c0a3c5ab8e167e8d27e1e2c88d3423a0
Ankit Kumar <anshkrrr@gmail.com>
2021-11-13 00:35:46 +0530

    Update floyd-warshall.cc

--- GFG/Graph/floyd-warshall.cc
+++ GFG/Graph/floyd-warshall.cc
@@ -13,4 +13,21 @@ public:
 	    }
 	}
 };
+class Solution {
+  public:
+	void shortest_distance(vector<vector<int>>&matrix){
+	    int n = matrix.size();
+	    for(int k=0;k<n;k++){
+	        for(int i=0;i<n;i++){
+	            for(int j=0;j<n;j++){
+	                if(matrix[i][k]==-1 or matrix[k][j]==-1) continue;
+	               if(matrix[i][j]==-1) 
+	                    matrix[i][j] = matrix[i][k] + matrix[k][j];
+	               if(matrix[i][k] + matrix[k][j] < matrix[i][j])
+	                    matrix[i][j] = matrix[i][k] + matrix[k][j];
+	            }
+	        }
+	    }
+	}
+};
 Expected Time Complexity: O(V^3)
