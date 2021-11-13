183a7e1f1f53e383a9f9aa81101be2188bd7112b
Ankit Kumar <anshkrrr@gmail.com>
2021-11-13 18:57:11 +0530

    Create merge-two-bst.cc

---
+++ GFG/BinarySearchTree/merge-two-bst.cc
@@ -0,0 +1,36 @@
+class Solution{
+   public:
+    //Function to return a list of integers denoting the node 
+    //values of both the BST in a sorted order.
+    void inorder(Node* root,vector<int> &v){
+        if(!root) return ;
+        
+        inorder(root->left,v);
+        v.push_back(root->data);
+        inorder(root->right,v);
+    }
+    vector<int> merge(vector<int> &a,vector<int> &b){
+        int i=0; int j=0; int n = a.size(); int m = b.size();
+        vector<int> ans;
+        while(i<n and j<m){
+            if(a[i]<=b[j]) ans.push_back(a[i]),i++;
+            else if(b[j]<a[i]) ans.push_back(b[j]),j++;
+        }
+        if(i==n){
+            while(j<m) ans.push_back(b[j++]);
+        }
+        if(j==m){
+            while(i<n) ans.push_back(a[i++]);
+        }
+        return ans;
+    }
+    vector<int> merge(Node *root1, Node *root2)
+    {
+        vector<int> v1,v2;
+       inorder(root1,v1);
+       inorder(root2,v2);
+       
+       return merge(v1,v2);
+    }
+};
+Expected Time Complexity: O(M+N) where M and N are the sizes if the two BSTs.
