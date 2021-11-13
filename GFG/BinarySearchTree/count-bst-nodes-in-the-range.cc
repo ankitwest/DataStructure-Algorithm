f2a85de5ec2458047f4315cb0ad759cd4ea70e19
Ankit Kumar <anshkrrr@gmail.com>
2021-11-13 14:21:12 +0530

    Update and rename count -bst-nodes-in-the-range.cc to count-bst-nodes-in-the-range.cc

--- GFG/BinarySearchTree/count-bst-nodes-in-the-range.cc
+++ GFG/BinarySearchTree/count-bst-nodes-in-the-range.cc
@@ -17,3 +17,21 @@ int getCount(Node *root, int l, int h)
 }
 };
 Expected Time Complexity: O(N)
+
+  
+  
+  // M-2
+class Soluiton{
+public:
+  int getCount(Node *root, int l, int h)
+{
+    if(!root) return 0;
+       
+    if(root->data<l)
+        return getCount(root->right,l,h);
+    else if(root->data>h)
+        return getCount(root->left,l,h);
+    
+    return 1+getCount(root->left,l,h)+getCount(root->right,l,h);
+}
+};
