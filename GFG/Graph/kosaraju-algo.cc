981c15059667005a57216a97d4d7cd230faaa165
Ankit Kumar <anshkrrr@gmail.com>
2021-11-15 02:55:47 +0530

    Create kosaraju-algo.cc

---
+++ GFG/Graph/kosaraju-algo.cc
@@ -0,0 +1,45 @@
+class Solution{
+  public:
+  	//Function to find number of strongly connected components in the graph.
+    void dfs(int node,vector<int> &vis,vector<int> adj[],stack<int> &st){
+        vis[node]=1;
+        for(auto x:adj[node])
+            if(!vis[x])
+                dfs(x,vis,adj,st);
+       
+        st.push(node);
+    }
+
+    void revdfs(int node,vector<int> &vis,vector<int> transpose[]){
+        vis[node]=1;
+        for(auto x:transpose[node])
+            if(!vis[x])
+                revdfs(x,vis,transpose);
+       
+    }
+    int kosaraju(int V, vector<int> adj[])
+    {
+        stack<int> st;
+        int n=V;
+        vector<int> vis(V,0);
+        for(int i=0;i<n;i++){
+            if(!vis[i])
+            dfs(i,vis,adj,st);
+        }
+        vector<int> transpose[V];
+        for(int i=0;i<n;i++){
+            vis[i]=0;
+            for(auto x:adj[i])
+                transpose[x].push_back(i);
+        }
+        int scc=0;
+        while(!st.empty()){
+            int node = st.top();st.pop();
+                if(!vis[node]){
+                    scc++;revdfs(node,vis,transpose);
+                }
+        }
+         return scc;
+    }
+};
+Expected Time Complexity: O(V+E).
