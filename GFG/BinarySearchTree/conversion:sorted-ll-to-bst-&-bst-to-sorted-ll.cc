a82637bfe29983e9d4bcb6c1993812a227792480
Ankit Kumar <anshkrrr@gmail.com>
2021-11-15 09:09:38 +0530

    Update and rename conversion:sorted-ll-to-bst.cc to conversion:sorted-ll-to-bst-&-bst-to-sorted-ll.cc

--- GFG/BinarySearchTree/conversion:sorted-ll-to-bst-&-bst-to-sorted-ll.cc
+++ GFG/BinarySearchTree/conversion:sorted-ll-to-bst-&-bst-to-sorted-ll.cc
@@ -1,3 +1,4 @@
+//Sorted LL to BST
 class Solution{
     public:
     TNode* constructBST(LNode* &head,int n){
@@ -24,10 +25,48 @@ Expected Time Complexity: O(N), N = number of Nodes
 
 
 Linked List: 1->2->3->4->5->6->7
-      4
+        4
       /   \
      2     6
    /  \   / \
   1   3  5   7  
-    preorder traversal of this 
+ Hence,   preorder traversal of this 
 tree is 4 2 1 3 6 5 7
+    
+    
+    
+    
+    
+    
+    
+// Flatten a BST to sorted list
+ class Solution{
+public:
+    void inorder(Node* root,Node* &prev){
+    if(!root) return ;
+
+    inorder(root->left,prev);
+    prev->left=NULL;
+    prev->right=root->data;
+    prev=root;
+    inorder(root->right,prev);
+ }
+
+    Node* flatten(Node* root){
+        Node* dummy = new Node(-1);
+        Node* prev = dummy;
+        inorder(root,prev);
+        prev->left=NULL;
+        prev->right=NULL;
+
+        return dummy->right;
+    }
+};
+
+Input: 
+          5 
+        /   \ 
+       3     7 
+      / \   / \ 
+     2   4 6   8
+Output: 2 3 4 5 6 7 8
