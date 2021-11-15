c7da7de0f2ca1362f68532fc0595d7610d12a15d
Ankit Kumar <anshkrrr@gmail.com>
2021-11-15 12:07:51 +0530

    Update prims-algo.cc

--- Striver/Graph/prims-algo.cc
+++ Striver/Graph/prims-algo.cc
@@ -51,3 +51,128 @@ int main(){
         cout << parent[i] << " - " << i <<" \n"; 
 	return 0;
 }
+
+
+
+
+///Efficient
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int N,m;
+	cin >> N >> m;
+	vector<pair<int,int> > adj[N]; 
+
+	int a,b,wt;
+	for(int i = 0; i<m ; i++){
+		cin >> a >> b >> wt;
+		adj[a].push_back(make_pair(b,wt));
+		adj[b].push_back(make_pair(a,wt));
+	}	
+	
+	int parent[N]; 
+      
+    int key[N]; 
+      
+    bool mstSet[N]; 
+  
+    for (int i = 0; i < N; i++) 
+        key[i] = INT_MAX, mstSet[i] = false; 
+    
+    priority_queue< pair<int,int>, vector <pair<int,int>> , greater<pair<int,int>> > pq;
+
+    key[0] = 0; 
+    parent[0] = -1; 
+    pq.push({0, 0});
+    // Run the loop till all the nodes have been visited
+    // because in the brute code we checked for mstSet[node] == false while computing the minimum
+    // but here we simply take the minimal from the priority queue, so a lot of times a node might be taken twice
+    // hence its better to keep running till all the nodes have been taken. 
+    // try the following case: 
+    // Credits: Srejan Bera
+    // 6 7 
+    // 0 1 5 
+    // 0 2 10 
+    // 0 3 100 
+    // 1 3 50 
+    // 1 4 200
+    // 3 4 250
+    // 4 5 50 
+    while(!pq.empty())
+    { 
+        int u = pq.top().second; 
+        pq.pop(); 
+        
+        mstSet[u] = true; 
+        
+        for (auto it : adj[u]) {
+            int v = it.first;
+            int weight = it.second;
+            if (mstSet[v] == false && weight < key[v]) {
+                parent[v] = u;
+		key[v] = weight; 
+                pq.push({key[v], v});    
+            }
+        }
+            
+    } 
+    
+    for (int i = 1; i < N; i++) 
+        cout << parent[i] << " - " << i <<" \n"; 
+	return 0;
+}
+
+
+
+// GFG
+
+// THE GFG practice section problem has faulty test case, it says the nodes are 0-based, but it has 0-based nodes and also has nodes which are names as N. 
+// Hence be careful while submitting the code at GFG
+// THe below code runs fine. 
+
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int N, vector<vector<int>> adj[])
+    {
+        int parent[N+1]; 
+          
+        int key[N+1]; 
+          
+        bool mstSet[N+1]; 
+      
+        for (int i = 0; i <= N; i++) 
+            key[i] = INT_MAX, mstSet[i] = false; 
+        
+        priority_queue< pair<int,int>, vector <pair<int,int>> , greater<pair<int,int>> > pq;
+    
+        key[0] = 0; 
+        parent[0] = -1; 
+        pq.push({0, 0});
+        while(!pq.empty())
+        { 
+            int u = pq.top().second; 
+            pq.pop(); 
+            
+            mstSet[u] = true; 
+            
+            for (auto it : adj[u]) {
+                int v = it[0];
+                int weight = it[1];
+                if (mstSet[v] == false && weight < key[v]) {
+                    parent[v] = u;
+    		key[v] = weight; 
+                    pq.push({key[v], v});    
+                }
+            }
+                
+        }
+        int sum = 0;
+        for(int i = 1;i<=N;i++) {
+            if(key[i] != INT_MAX) sum += key[i];
+        }
+        return sum; 
+    }
+};
