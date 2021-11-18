70fbc45027885198373dbead2f314326a93695ba
Ankit Kumar <anshkrrr@gmail.com>
2021-11-18 18:11:34 +0530

    Create circular-tour-petrol.cc

---
+++ GFG/Stack/Queue/circular-tour-petrol.cc
@@ -0,0 +1,19 @@
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+class Solution{
+public:
+    int tour(petrolPump p[],int n)
+    {
+       int req_petrol=0,curr_petrol=0,start=0;
+        for(int i=0;i<n;i++){
+        curr_petrol += p[i].petrol-p[i].distance;
+        if(curr_petrol<0){
+            req_petrol += curr_petrol;
+            curr_petrol=0;
+            start=i+1;
+            }
+        }
+    return (req_petrol+curr_petrol>=0?start:-1);
+    }
+};
+Expected Time Complexity: O(N)
