a528ba5bb09f7ee5d559af6c9abb4c70eeaefa95
Ankit Kumar <anshkrrr@gmail.com>
2021-11-18 08:55:38 +0530

    Create valid-parenthesis.cc

---
+++ GFG/Bracket/valid-parenthesis.cc
@@ -0,0 +1,24 @@
+class Solution{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+    map<char,char> m;
+        stack<char> st;
+        m[')']='(';
+        m['}']='{';
+        m[']']='[';
+        st.push(x[0]);
+        
+        for(int i=1;i<x.size();i++){
+            char ch = x[i];
+            if(!st.empty() and st.top()==m[ch]) 
+                st.pop();
+            else  
+                st.push(ch);
+        }
+        if(st.empty()) return true;
+        else return false;
+    }
+};
+Expected Time Complexity: O(|x|)
