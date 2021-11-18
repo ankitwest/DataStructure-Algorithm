9f838c463ba857494afafa85d00cba0ce4ae3ff9
Ankit Kumar <anshkrrr@gmail.com>
2021-11-18 17:34:56 +0530

    Create maximum-of-all-subarryas-of-size-k.cc

---
+++ GFG/Stack/Set/maximum-of-all-subarryas-of-size-k.cc
@@ -0,0 +1,20 @@
+class Solution{
+public:
+    //Function to find maximum of each subarray of size k.
+    vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+        vector<int> v;
+        multiset<int,greater<int>> s;
+        for(int i=0;i<n;i++){
+            s.insert(arr[i]);
+            if(s.size()>k){
+                s.erase(s.find(arr[i-k]));
+            }
+            if(s.size()==k){
+                v.push_back(*s.begin());
+            }
+        }
+        return v;
+    }
+};
+Expected Time Complexity: O(N)
