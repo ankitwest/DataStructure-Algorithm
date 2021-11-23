c047a0887e5d21c29d89a7b90a7baf0a625462f2
Ankit Kumar <anshkrrr@gmail.com>
2021-11-23 16:58:34 +0530

    Update segment-tree.cc

--- GFG/segment-tree.cc
+++ GFG/segment-tree.cc
@@ -52,3 +52,49 @@ int main(){
 	}
 	return 0;
 }
+
+
+
+
+
+// Maximum of two node
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[1000005],seg[10000005];
+void build(int ind,int low,int high){
+	if(low==high){
+		seg[ind]=a[low];
+		return ;
+	}
+	int mid = (low+high)/2;
+	build(2*ind+1,low,high);
+	build(2*ind+2,low,high);
+
+	seg[ind]=max(seg[ind+1],seg[2*ind+2]);
+}
+
+int query(int ind,int low,int high,int l, int r){
+	if(low>=l and high<=r){
+		return seg[ind];
+	}
+	if(high<l or low>r) return INT_MIN;
+	int mid = (low + high)/2;
+	int left = query(2*ind+1,low,mid,l,r);
+	int right = query(2*ind+2,mid+1,high,l,r);
+	return max(left,right);
+}
+
+int main(){
+	int n;cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+	build(0,0,n-1);
+	int q;cin>>q;
+	while(q--){
+		int l,r;cin>>l>>r;
+		cout<<(query(0,0,n-1,l,r))<<endl;
+	}
+}
