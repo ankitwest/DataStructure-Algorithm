6d1a5030980f7719ca2f9c750e6b506ffdea6a1d
Ankit Kumar <anshkrrr@gmail.com>
2021-12-07 23:39:18 +0530

    Create 0518. Coin Change 2.cc

---
+++ LeetCodeSolution/0518. Coin Change 2.cc
@@ -0,0 +1,11 @@
+Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+
+
+
+Input: amount = 5, coins = [1,2,5]
+Output: 4
+Explanation: there are four ways to make up the amount:
+5=5
+5=2+2+1
+5=2+1+1+1
+5=1+1+1+1+1
