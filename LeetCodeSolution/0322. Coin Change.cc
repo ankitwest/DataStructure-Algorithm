d3c4e5f1a4fba190f7053a183a8a0a3303358bf5
Ankit Kumar <anshkrrr@gmail.com>
2021-12-08 19:57:52 +0530

    Update 0322. Coin Change.cc

--- LeetCodeSolution/0322. Coin Change.cc
+++ LeetCodeSolution/0322. Coin Change.cc
@@ -4,15 +4,14 @@ You may assume that you have an infinite number of each kind of coin.
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amt) {
-           int Max = amt + 1 ;
+           int Max = amt + 1;
         vector<int> dp(amt+1, Max);
+         dp[0]=0;
         
-          dp[0]=0;
-      
-        for(int i = 1 ; i <= amt; i++){
-        for(int j = 0 ; j < coins.size() ; j++){
-    		if(i >=coins[j])
-    			dp[i]  = min(dp[i],dp[i-coins[j]] +1);
+        for(int val=1;val<=amt;val++){
+         for(auto coin:coins){
+    		if(val >=coin)
+    			dp[val]  = min(dp[val],dp[val-coin]+1);
            }
       }
     return dp[amt] > amt ? -1 : dp[amt];
