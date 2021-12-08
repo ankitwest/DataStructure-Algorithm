c8b76a8c5e1b6c3349713c800d9bb5b70cc3e430
Ankit Kumar <anshkrrr@gmail.com>
2021-12-08 19:50:26 +0530

    Update 0518. Coin Change 2.cc

--- LeetCodeSolution/0518. Coin Change 2.cc
+++ LeetCodeSolution/0518. Coin Change 2.cc
@@ -1,6 +1,21 @@
 Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
 
 
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int dp[amount+1];
+        memset(dp,0,sizeof(dp));
+        dp[0]=1;
+        for(auto coin:coins){
+            for(int val=1;val<=amount;val++){
+                if(val>=coin) 
+                    dp[val]+=dp[val-coin];
+            }
+        }
+        return dp[amount];
+    }
+};
 
 Input: amount = 5, coins = [1,2,5]
 Output: 4
