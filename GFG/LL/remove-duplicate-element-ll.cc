a3a208d5adb727923417d6112794eb095c4d9261
Ankit Kumar <anshkrrr@gmail.com>
2021-12-09 00:35:07 +0530

    Update remove-duplicate-element-ll.cc

--- GFG/LL/remove-duplicate-element-ll.cc
+++ GFG/LL/remove-duplicate-element-ll.cc
@@ -1,16 +1,45 @@
-Node *removeDuplicates(Node *head)
+
+// Sorted list
+class Solution
 {
-    if(!head) return NULL;
-    
-     Node* curr = head;
-    //  Node* temp = NULL;
-     while(curr->next!=NULL){
-         if(curr->data == curr->next->data){
-            //  temp = curr->next;
-             curr->next = curr->next->next;
-            //  delete temp;
+    public:
+    Node *removeDuplicates(Node *head)
+    {
+        if(!head) return NULL;
+
+         Node* curr = head;
+        //  Node* temp = NULL;
+         while(curr->next!=NULL){
+             if(curr->next->data == curr->data){
+                //  temp = curr->next;
+                 curr->next = curr->next->next;
+                //  delete temp;
+             }
+             else curr = curr->next;
          }
-         else curr = curr->next;
-     }
-     return head;
-}
+         return head;
+    }
+];
+
+
+// Unsorted list
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node * removeDuplicates( Node *head) 
+    {
+        if(!head) return NULL;
+        Node* curr = head;  
+        unordered_map<int,int> m;
+        m[curr->data]=1;
+        while(curr->next!=NULL){
+            int x = curr->next->data;
+            if(m.find(x)!=m.end())
+                curr->next = curr->next->next;
+            else
+                m[x]=1, curr = curr->next;
+        }
+        return head;
+    }
+};
