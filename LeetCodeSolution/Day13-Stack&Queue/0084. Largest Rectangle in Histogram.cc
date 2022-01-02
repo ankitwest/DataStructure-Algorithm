87d91c876a8d479d3ddf70a256b985115af872c0
Ankit Kumar <anshkrrr@gmail.com>
2022-01-02 20:02:38 +0530

    Create 0084. Largest Rectangle in Histogram.cc

---
+++ LeetCodeSolution/Day13-Stack&Queue/0084. Largest Rectangle in Histogram.cc
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st; int n = heights.size();
+        vector<int>left(n),right(n);
+        
+        for(int i=0;i<n;i++){
+            while(!st.empty() and heights[st.top()]>=heights[i])
+                st.pop();
+            if(st.empty()) left[i]=0;
+            else left[i]=st.top()+1;
+            st.push(i);
+        }
+        
+        while(!st.empty()) st.pop();
+        
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() and heights[st.top()]>=heights[i])
+                st.pop();
+            if(st.empty()) right[i]=n-1;
+            else right[i]=st.top()-1;
+            st.push(i);
+        }
+        
+        int max_area = 0;
+        for(int i=0;i<n;i++){
+            max_area=max(max_area,heights[i]*(right[i]-left[i]+1));
+        }
+        return max_area;
+    }
+};
