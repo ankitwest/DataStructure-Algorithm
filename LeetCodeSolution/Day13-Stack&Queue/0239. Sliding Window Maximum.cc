ca2f2a371ac3ae39af64d6fea35eef9c159a09d6
Ankit Kumar <anshkrrr@gmail.com>
2022-01-02 23:01:56 +0530

    Update 0239. Sliding Window Maximum.cc

--- LeetCodeSolution/Day13-Stack&Queue/0239. Sliding Window Maximum.cc
+++ LeetCodeSolution/Day13-Stack&Queue/0239. Sliding Window Maximum.cc
@@ -12,3 +12,24 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq; int n = nums.size();
+        vector<int> ans;
+        
+        for(int i=0;i<n;i++){
+            if(!dq.empty() and dq.front()==i-k) dq.pop_front(); //only window size of k is allowed
+            
+            while(!dq.empty() and nums[dq.back()]<=nums[i])
+                dq.pop_back();
+            dq.push_back(i);
+            
+            if(i+1>=k) ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
+
