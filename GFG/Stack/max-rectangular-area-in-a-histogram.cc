00a8d1adf473bd48eb4181fe95e9a0fc45a38f1a
Ankit Kumar <anshkrrr@gmail.com>
2022-01-02 20:20:00 +0530

    Update max-rectangular-area-in-a-histogram.cc

--- GFG/Stack/max-rectangular-area-in-a-histogram.cc
+++ GFG/Stack/max-rectangular-area-in-a-histogram.cc
@@ -4,17 +4,19 @@ public:
     long long getMaxArea(long long arr[], int n)
     {
         stack<int> st;
-        stack<int> stLeft,stRight;
         vector<int> left(n),right(n);
+        
         for(int i=0;i<n;i++){
-            while(!st.empty() and arr[st.top()]>=arr[i]){
+            while(!st.empty() and arr[st.top()]>=arr[i]){     // we are looking for such index which could yield me a height less than the current one 
                 st.pop();
             }
             if(st.empty()) left[i]=0;
             else left[i]=st.top()+1;   // stopping one element to the right
             st.push(i);
         }
+        
         while(!st.empty()) st.pop();
+        
         for(int i=n-1;i>=0;i--){
             while(!st.empty() and arr[st.top()]>=arr[i]){
                 st.pop();
@@ -23,6 +25,7 @@ public:
             else right[i]=st.top()-1;   // stopping one element to the left 
             st.push(i);
         }
+        
          long long max_area=0;
        for(int i=0;i<n;i++){
            max_area=max(max_area,arr[i]*(right[i]-left[i]+1));   // as we calculate the width
