ddb0635e7c15a74f1ad1bc17bc16b9032efb51d7
Ankit Kumar <anshkrrr@gmail.com>
2022-01-11 15:23:59 +0530

    Update 0322. Coin Change.cc

--- LeetCodeSolution/0322. Coin Change.cc
+++ LeetCodeSolution/0322. Coin Change.cc
@@ -21,3 +21,28 @@ public:
 Input: coins = [1,2,5], amount = 11
 Output: 3
 Explanation: 11 = 5 + 5 + 1
+
+    
+    
+    GFG Solution  
+    bettter and simple one 
+    
+ class Solution {   
+    public:
+	int minCoins(int coins[], int n, int amt) 
+	{ 
+	   int maxi = amt+1;
+
+	   vector<int> dp(amt+1,maxi);
+	   dp[0] = 0;
+	   
+	   sort(coins,coins+n);
+	   
+	   for(int i=0;i<n;i++){
+	       for(int j=coins[i];j<=amt;j++){
+	           dp[j] = min(dp[j],1+dp[j-coins[i]]);
+	       }
+	   }
+	   return dp[amt]>amt?-1:dp[amt];
+	} 
+    };
