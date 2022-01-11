f30d13f7ca5e91263f196de26e491f8827d51afc
Ankit Kumar <anshkrrr@gmail.com>
2022-01-11 20:33:40 +0530

    Create KMP-algorithm.cc

---
+++ GFG/String/KMP-algorithm.cc
@@ -0,0 +1,55 @@
+1
+batmanandrobinarebat
+bat
+
+Your Output: 
+0 0 0 0 1 2 3 0 0 0 0 0 0 0 0 1 0 0 0 0 0 1 2 3    // lps
+1 18 
+  
+  
+  
+
+class Solution{
+   public:
+        vector <int> search(string pat, string txt)
+        {
+           vector<int> v;
+           string t = pat + "#" + txt;
+           int n = t.size();
+           int lps[n];
+           memset(lps,0,sizeof(lps));
+           int i=1,len = 0;
+           
+           while(i<n){
+               if(t[i]==t[len]){
+                   while(t[i]==t[len]){
+                       len++;
+                       lps[i] = len;
+                      i++;
+                   }
+               }else{
+                  if(len>0)
+                    len = lps[len-1];
+                    else{
+                        lps[i]=0;
+                        i++;
+                    }
+                    
+               }
+               
+           }
+        //   for(int i=0;i<n;i++) cout<<lps[i]<<" ";  cout<<endl;
+    
+        int m = pat.size();
+        for(int i=1;i<n;i++){
+            if(lps[i]==m)
+                v.push_back(i-2*m+1);
+        }
+        
+        if(v.size()) return v;
+        v.push_back(-1);
+        return v;
+        
+        }
+     
+};
