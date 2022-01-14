96353eac583411061c17fa5f55beac183a04f408
Ankit Kumar <anshkrrr@gmail.com>
2022-01-14 23:13:01 +0530

    Update #Matrix Chain Multiplication.cc

--- GFG/DP/#Matrix Chain Multiplication.cc
+++ GFG/DP/#Matrix Chain Multiplication.cc
@@ -45,3 +45,42 @@ named as A, B, C, D. Out of all possible combinations,
 the most efficient way is (A*(B*C))*D. 
 The number of operations are -
 20*30*10 + 40*20*10 + 40*10*30 = 26000.
+  
+  
+  
+  
+  
+  class Solution{
+  public:
+  int solve(int arr[],int n){
+        vector<vector<int>> dp(n-1,vector<int>(n-1,0));
+        int l = n-1;
+        for(int g=0;g<dp.size();g++){
+            for(int i=0,j=g;j<dp.size();i++,j++){
+                if(g==0)
+                    dp[i][j]=0;
+                 else if(g==1)
+                    dp[i][j]=arr[i]*arr[j+1]*arr[j];
+                else{
+                    int mini = INT_MAX;
+                    for(int k=i;k<j;k++){
+                        //left - i to k and right - k+1 to j
+                        int lc = dp[i][k];
+                        int rc = dp[k+1][j];
+                        int mc = arr[i]*arr[k+1]*arr[j+1];
+                        int tc = lc + rc + mc;
+                        mini = min(mini,tc);
+                    }
+                    dp[i][j] = mini;
+                }
+            }
+        }
+        
+        return dp[0][dp.size()-1];
+    }
+    
+    int matrixMultiplication(int n, int arr[])
+    {
+        return solve(arr,n);
+    }
+  };
