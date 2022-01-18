379babe6c8f6490a6ccc9d6cb9d59c361ac6efb4
Ankit Kumar <anshkrrr@gmail.com>
2022-01-18 16:51:19 +0530

    Update 0907. Sum of Subarray Minimums.cc

--- LeetCodeSolution/0907. Sum of Subarray Minimums.cc
+++ LeetCodeSolution/0907. Sum of Subarray Minimums.cc
@@ -57,3 +57,34 @@ Example 2:
 
 Input: arr = [11,81,94,43,3]
 Output: 444
+
+    
+    // Simpler form
+    
+    int n=nums.size();
+        long long sum=0;
+        stack<int>st;
+        vector<int> minPrev(n,-1),minNext(n,n);
+        
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty()&&nums[st.top()]>=nums[i]){st.pop();}
+            if(!st.empty()){minPrev[i]=st.top();}
+            st.push(i);
+        }
+        while(!st.empty()){st.pop();}
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.empty()&&nums[st.top()]>nums[i]){st.pop();}
+            if(!st.empty()){minNext[i]=st.top();}
+            st.push(i);
+        }
+
+         for(int i=0;i<n;i++)
+        {
+            long long leftMin=i-minPrev[i],rightMin=minNext[i]-i;
+            sum+=(leftMax*rightMaxx)*nums[i];
+            
+        }
+        return sum;
+    
