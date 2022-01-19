13068f3df8be815f4cf5229af296e647ff258978
Ankit Kumar <anshkrrr@gmail.com>
2022-01-19 10:24:58 +0530

    Update dsu2.cc

--- GFG/dsu2.cc
+++ GFG/dsu2.cc
@@ -55,3 +55,52 @@ class Solution
         return s.size();
     }
   };
+
+
+
+
+
+
+3. Detect Cycle using DSU 
+
+
+class Solution
+{
+    public:
+    //Function to detect cycle using DSU in an undirected graph.
+    int findPar(int x,int par[]){
+        if(x == par[x])
+            return x;
+        return par[x] = findPar(par[x],par);
+    }
+    bool unionCycle(int a,int b,int par[],int rank[]){
+        int ax = findPar(a,par);
+        int bx = findPar(b,par);
+        
+        if(ax==bx and a!=b) return true;
+        
+        if(rank[ax]>rank[bx]) par[bx] = ax;
+        if(rank[bx]>rank[ax]) par[ax] = bx;
+        else{
+            par[bx] = ax;  rank[ax]++;
+        }
+        return false;
+    }
+	int detectCycle(int V, vector<int>adj[])
+	{
+	    int rank[V];
+	    int par[V];
+	    
+	    for(int i=0;i<V;i++){
+	        par[i] = i; rank[i] = 0;
+	    }
+	    
+	    for(int node=0;node<V;node++){
+	        for(auto child : adj[node]){
+	            if(child>node and unionCycle(child,node,par,rank)==true)
+	                return 1;
+	        }
+	    }
+	    return 0;
+	}
+};
