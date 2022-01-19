5160e6f5ed168d277190ad51d95bfd79bfa99862
Ankit Kumar <anshkrrr@gmail.com>
2022-01-19 10:10:16 +0530

    Create dsu2.cc

---
+++ GFG/dsu2.cc
@@ -0,0 +1,57 @@
+
+GFG Q
+Disjoint set (Union-Find)
+
+
+int find(int parent[],int node)
+{
+      if(node==parent[node])
+        return node;
+        return parent[node] = find(parent,parent[node]);
+}
+void unionSet(int parent[],int u,int v)
+{
+	u = find(parent,u);
+	v = find(parent,v);
+	
+	if(u==v) return;
+	parent[u] = v;
+}
+
+
+
+
+
+
+
+
+class Solution
+{
+  public:
+    int findPar(int node,int parent[]){
+        if(node==parent[node])
+            return node;
+        return parent[node] = findPar(parent[node],parent);
+    }
+    void unionNodes( int a, int b, int parent[], int rank[], int n) 
+    {
+        a = findPar(a,parent);
+        b = findPar(b,parent);
+        
+        if(rank[a]>rank[b]) parent[b] = a;
+        else if(rank[b]>rank[a]) parent[a] = b;
+        else{
+            parent[b] = a;  rank[a]++;
+        }
+    }
+    
+    //Function to determine number of connected components.
+    int findNumberOfConnectedComponents( int n, int parent[], int rank[]) 
+    {
+        set<int> s;
+        for(int i=1;i<=n;i++){
+            s.insert(findPar(i,parent));
+        }
+        return s.size();
+    }
+  };
