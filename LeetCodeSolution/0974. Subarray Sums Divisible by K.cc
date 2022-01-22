3f4d1b8ae3fc04237bc5df61239efd488e74ea40
Ankit Kumar <anshkrrr@gmail.com>
2022-01-22 19:29:15 +0530

    Create 0974. Subarray Sums Divisible by K.cc

---
+++ LeetCodeSolution/0974. Subarray Sums Divisible by K.cc
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int ans=0; int sum=0; int n = nums.size();
+        unordered_map<int,int> m;
+        m.insert({0,1});
+        
+        for(int i=0;i<n;i++){
+            sum = ((sum + nums[i]) % k + k) % k;
+            // if(sum<0) sum += k;
+            if(m.find(sum)!=m.end()) ans += m[sum%k];
+            m[sum%k]++;
+        }
+        return ans;
+    }
+};
+
+Input: nums = [4,5,0,-2,-3,1], k = 5
+Output: 7
+Explanation: There are 7 subarrays with a sum divisible by k = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
