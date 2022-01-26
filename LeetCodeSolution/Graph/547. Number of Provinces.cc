0b8c4f8e21cf50b66a8a2934eeb07dc01626771d
Ankit Kumar <anshkrrr@gmail.com>
2022-01-26 22:22:46 +0530

    Update 547. Number of Provinces.cc

--- LeetCodeSolution/Graph/547. Number of Provinces.cc
+++ LeetCodeSolution/Graph/547. Number of Provinces.cc
@@ -36,3 +36,48 @@ public:
 
 Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
 Output: 2  
+    
+    
+    
+    
+    Unionn Find
+    class Solution {
+public:
+    int parent[201];
+    void make_set(int n){
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+        }
+    }
+    void UNION(int a,int b){
+        a = find_parent(a);
+        b = find_parent(b);
+        if(a!=b){
+            parent[b]=a;
+        }
+    }
+    int find_parent(int node){
+        if(node==parent[node]) return node;
+        return parent[node] = find_parent(parent[node]);
+    }
+    
+    int findCircleNum(vector<vector<int>>& matrix){
+        int n=matrix.size();
+        int m=matrix[0].size();
+            
+        make_set(n);
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==1)
+                    UNION(i,j);
+            }
+        }
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            if(parent[i]==i)
+                ans++;
+        }
+        return ans;
+    }
+};
