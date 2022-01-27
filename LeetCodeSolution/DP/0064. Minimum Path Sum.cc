9e41d9d05011dd4bb1536c026f48f3d412af77a1
Ankit Kumar <anshkrrr@gmail.com>
2022-01-27 22:02:37 +0530

    Update 0064. Minimum Path Sum.cc

--- LeetCodeSolution/DP/0064. Minimum Path Sum.cc
+++ LeetCodeSolution/DP/0064. Minimum Path Sum.cc
@@ -26,3 +26,28 @@ public:
 Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
 Output: 7
 Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+
+    
+    class Solution {
+public:
+    vector<vector<int>> dp;
+    int n;int m;
+    int solve(vector<vector<int>>& grid,int i,int j){
+        if(i==n-1 and j==m-1)
+            return grid[i][j];
+        if(i>=n or j>=m)
+            return 1e6;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        int curr = grid[i][j] + min(solve(grid,i+1,j),solve(grid,i,j+1));
+        
+        return dp[i][j] = curr;
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        n = grid.size();m = grid[0].size();
+        dp.resize(n,vector<int>(m,-1));
+        
+        return  solve(grid,0,0);
+    }
+};
