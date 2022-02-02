e4ecf6a67cbff90b19d4cfecfb4eb59b0ef18980
Ankit Kumar <anshkrrr@gmail.com>
2022-02-02 22:53:50 +0530

    Update coin-change.cc

--- GFG/DP/coin-change.cc
+++ GFG/DP/coin-change.cc
@@ -23,3 +23,28 @@ class Solutin{
         return dp[amt];    
     }  
 };
+
+
+
+Q find min no of coins to make up target
+
+
+ class Solution {   
+    public:
+	int minCoins(int coins[], int n, int amt) 
+	{ 
+	   int maxi = amt+1;
+
+	   vector<int> dp(amt+1,maxi);
+	   dp[0] = 0;
+	   
+	   sort(coins,coins+n);
+	   
+	   for(int i=0;i<n;i++){
+	       for(int j=coins[i];j<=amt;j++){
+	           dp[j] = min(dp[j],1+dp[j-coins[i]]);
+	       }
+	   }
+	   return dp[amt]>amt?-1:dp[amt];
+	} 
+};
