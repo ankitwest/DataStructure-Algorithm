65473e7dbe15a78a70f14c0bc488d054b5ff35db
Ankit Kumar <anshkrrr@gmail.com>
2022-02-02 19:13:43 +0530

    Create 0456. 132 Pattern.cc

---
+++ LeetCodeSolution/0456. 132 Pattern.cc
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n = nums.size();
+        int minVal[n]; minVal[0] = nums[0];
+        for(int i=1;i<n;i++){
+            minVal[i] = min(minVal[i-1],nums[i]);
+        }
+        stack<int> st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() and st.top()<=minVal[i])
+                st.pop();
+            
+            if(!st.empty() and st.top()<nums[i]) return true;
+            
+            st.push(nums[i]);
+        }
+        return false;
+    }
+};
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: false
+Explanation: There is no 132 pattern in the sequence.
+Example 2:
+
+Input: nums = [3,1,4,2]
+Output: true
+Explanation: There is a 132 pattern in the sequence: [1, 4, 2].
