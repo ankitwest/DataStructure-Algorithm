b8c59da1f13788e2be2437a65fe9e62ac489d56c
Ankit Kumar <anshkrrr@gmail.com>
2022-02-03 13:16:32 +0530

    Update 0684. Redundant Connection.cc

--- LeetCodeSolution/Graph/0684. Redundant Connection.cc
+++ LeetCodeSolution/Graph/0684. Redundant Connection.cc
@@ -42,3 +42,40 @@ Example 2:
 
 Input: edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
 Output: [1,4]
+
+
+
+DSU
+
+
+int find_par(int node,vector<int> &parent){
+        if(node==parent[node])
+            return node;
+        else return parent[node]= find_par(parent[node],parent);
+    }
+    
+    void UNION(int a,int b,vector<int> &parent){
+        int x = find_par(a,parent);
+        int y = find_par(b,parent);
+        
+        parent[x]=y;
+    }
+    
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<int> parent(1005);
+        for(int i=0;i<1005;i++)
+            parent[i]=i;
+        
+        vector<int> ans;
+        for(int i=0;i<edges.size();i++){
+            int u = edges[i][0]; int v = edges[i][1];
+            int x = find_par(u,parent);
+            int y = find_par(v,parent);
+            
+            if(x==y)
+                ans = {u,v};
+            else 
+                UNION(u,v,parent);
+        }
+        return ans;
+    }
