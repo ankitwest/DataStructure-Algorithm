96eb3205ae2c9a7f429d20bca25391e87c41144f
Ankit Kumar <anshkrrr@gmail.com>
2022-02-03 19:22:09 +0530

    Create 0454. 4Sum II.cc

---
+++ LeetCodeSolution/HashMap/0454. 4Sum II.cc
@@ -0,0 +1,33 @@
+
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int,int> m;
+        for(auto a:nums1){
+            for(auto b:nums2){
+                m[a+b]++;
+            }
+        }
+        
+        int ans=0;
+        for(auto c:nums3){
+            for(auto d:nums4){
+                if(m.find(-c-d)!=m.end())
+                    ans += m[-c-d];
+            }
+        }
+        return ans;
+    }
+};
+Example 1:
+
+Input: nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+Output: 2
+Explanation:
+The two tuples are:
+1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1) + 0 = 0
+Example 2:
+
+Input: nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+Output: 1
