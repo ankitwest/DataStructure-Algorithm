b312fcae39b0faf1d5f70984562b36dd4134067a
Ankit Kumar <anshkrrr@gmail.com>
2022-02-03 19:08:57 +0530

    Create 0001. Two Sum.cc

---
+++ LeetCodeSolution/HashMap/0001. Two Sum.cc
@@ -0,0 +1,20 @@
+    
+//Two Sum
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            int no1= nums[i];
+            int no2= target- no1;
+            if(m.find(no2)!=m.end()){
+                int j =  m[no2];
+                ans.push_back(i),ans.push_back(j);
+            }
+            m[no1]=i;
+        }
+        return ans;
+    }
+};
