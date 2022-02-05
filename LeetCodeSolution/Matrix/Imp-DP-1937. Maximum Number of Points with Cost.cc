303dcb78377d3a3918b35bb7b8ec1fbc58998e74
Ankit Kumar <anshkrrr@gmail.com>
2022-02-05 12:14:32 +0530

    Update Imp-DP-1937. Maximum Number of Points with Cost.cc

--- LeetCodeSolution/Matrix/Imp-DP-1937. Maximum Number of Points with Cost.cc
+++ LeetCodeSolution/Matrix/Imp-DP-1937. Maximum Number of Points with Cost.cc
@@ -39,3 +39,23 @@ public:
         return maxi;
     }
 };
+
+
+
+Input: points = [[1,2,3],[1,5,1],[3,1,1]]
+Output: 9
+Explanation:
+The blue cells denote the optimal cells to pick, which have coordinates (0, 2), (1, 1), and (2, 0).
+You add 3 + 5 + 3 = 11 to your score.
+However, you must subtract abs(2 - 1) + abs(1 - 0) = 2 from your score.
+Your final score is 11 - 2 = 9.
+    
+    
+    
+    Input: points = [[1,5],[2,3],[4,2]]
+Output: 11
+Explanation:
+The blue cells denote the optimal cells to pick, which have coordinates (0, 1), (1, 1), and (2, 0).
+You add 5 + 3 + 4 = 12 to your score.
+However, you must subtract abs(1 - 1) + abs(1 - 0) = 1 from your score.
+Your final score is 12 - 1 = 11.
