6eac147cfa161a86a246ac74c9e4911f239f2052
Ankit Kumar <anshkrrr@gmail.com>
2022-02-05 12:18:03 +0530

    Update 0064. Minimum Path Sum.cc

--- LeetCodeSolution/Matrix/0064. Minimum Path Sum.cc
+++ LeetCodeSolution/Matrix/0064. Minimum Path Sum.cc
@@ -30,3 +30,33 @@ Example 2:
 
 Input: grid = [[1,2,3],[4,5,6]]
 Output: 12
+
+    
+    
+    
+    M-2
+    class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size(); int m = grid[0].size();
+        vector<vector<int>> dp(n,vector<int>(m));
+        
+        //base case
+        dp[n-1][m-1] = grid[n-1][m-1];
+        
+        for(int i=n-2;i>=0;i--){
+            dp[i][m-1] = grid[i][m-1] + dp[i+1][m-1];
+        }
+        
+        for(int j=m-2;j>=0;j--){
+            dp[n-1][j] = grid[n-1][j] + dp[n-1][j+1];
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j=m-2;j>=0;j--){
+                 dp[i][j] = min(dp[i+1][j],dp[i][j+1]) + grid[i][j];
+            }
+        }
+        return dp[0][0];
+    }
+};
