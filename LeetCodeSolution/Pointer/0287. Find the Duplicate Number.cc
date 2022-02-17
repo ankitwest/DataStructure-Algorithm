1069b8a67fca496b2d3633c2c0dcd7af4933bb7a
Ankit Kumar <anshkrrr@gmail.com>
2022-02-17 20:47:47 +0530

    Create 0287. Find the Duplicate Number.cc

---
+++ LeetCodeSolution/Pointer/0287. Find the Duplicate Number.cc
@@ -0,0 +1,71 @@
+
+class Solution {
+public:
+      int findDuplicate(vector<int>& nums) {
+        int n=nums.size();
+          map<int,int>m;
+        for(int i=0;i<n;i++){
+            m[nums[i]]++;   
+        }
+          for(auto x:m)
+              if(x.second>1)
+                  return x.first;
+          
+        return 0;
+
+      }
+};
+
+ TC- O(NlogN)
+  SC -0(N)
+   
+   
+   M-2
+class Solution {
+public:
+      int findDuplicate(vector<int>& nums) {
+          int n = nums.size();
+          vector<int> freq(n+1,0);
+          for(int i=0;i<n;i++){
+              if(freq[nums[i]]==1) return nums[i];
+              else freq[nums[i]]++;
+          }
+          return -1;
+    }
+ };
+Runtime: 135 ms, 
+  TC- O(N)
+  SC -0(N)
+
+    
+    
+M-3
+    
+class Solution {
+public:
+      int findDuplicate(vector<int>& nums) {
+        int slow = nums[0],fast = nums[0];
+          
+          do{
+              slow = nums[slow];
+              fast = nums[nums[fast]];
+          }while(slow!=fast);
+          
+          fast = nums[0];
+          
+          while(slow!=fast){
+              slow = nums[slow];
+              fast = nums[fast];
+          }
+          
+          return slow;
+    }
+};
+Runtime: 178 ms, 
+
+    
+    
+    
+    
+  TC- O(N)
+  SC -0(1)
