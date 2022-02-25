d9625fe8e609dd53d016632989f5632d4f755d44
Ankit Kumar <anshkrrr@gmail.com>
2022-02-25 23:38:43 +0530

    Create 0165. Compare Version Numbers.cc

---
+++ LeetCodeSolution/Pointer/0165. Compare Version Numbers.cc
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int compareVersion(string v1, string v2) {
+        int n1 = v1.size(); int n2 = v2.size();
+        int i=0,j=0;  int num1=0; int num2=0;
+        
+        while(i<n1 or j<n2){
+            while(i<n1 and v1[i]!='.'){
+                num1 = num1*10 + (v1[i]-'0');
+                i++;
+            }
+            while(j<n2 and v2[j]!='.'){
+                num2 = num2*10 + (v2[j]-'0');
+                j++;
+            }
+            if(num1<num2) return -1;
+            if(num1>num2) return 1;
+            
+            num1=0,num2=0;
+            i++,j++;
+        }
+        return 0;
+    }
+};
