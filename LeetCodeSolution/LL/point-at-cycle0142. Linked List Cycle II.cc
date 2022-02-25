da4c85c25fd4647c3d322bb436790e0f0b81e355
Ankit Kumar <anshkrrr@gmail.com>
2022-02-26 00:22:11 +0530

    Create point-at-cycle0142. Linked List Cycle II.cc

---
+++ LeetCodeSolution/LL/point-at-cycle0142. Linked List Cycle II.cc
@@ -0,0 +1,33 @@
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head) return NULL;
+        ListNode* slow=head;ListNode* fast=head;
+        ListNode* prev=NULL; 
+        
+        while(fast and fast->next){
+            prev=slow,slow=slow->next,fast=fast->next->next;
+            if(slow==fast) break;
+        }
+        if(fast==NULL or fast->next==NULL) return NULL;
+        
+        fast = head;
+        while(slow!=fast){
+             prev=slow,slow=slow->next,fast=fast->next;
+        }
+        return prev->next;
+    }
+};
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
