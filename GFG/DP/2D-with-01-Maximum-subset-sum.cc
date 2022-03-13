6dfdec07ab9c22a21841b5d670ecc5f644e58c19
Ankit Kumar <anshkrrr@gmail.com>
2022-03-14 00:08:18 +0530

    Create 2D-with-01-Maximum-subset-sum.cc

---
+++ GFG/DP/2D-with-01-Maximum-subset-sum.cc
@@ -0,0 +1,36 @@
+class Solution
+{
+    public:
+    long long findMaxSubsetSum(long long A[], int N)
+    {
+        long long dp[N+1][2];
+        for(int i=0;i<=N;i++) dp[i][0] = dp[i][1] = INT_MIN;
+        
+        dp[0][0] = dp[0][1] = 0;
+        // dp[1][0] = 0;  dp[1][1] = A[0];
+        // dp[2][0] = A[0]; dp[2][1] = A[1];
+        
+        for(int i=1;i<=N;i++){
+            dp[i][0] = dp[i-1][1] + A[i-1];
+            dp[i][1] = max(dp[i-1][1] + A[i-1], dp[i-1][0] );
+        }
+        
+        return max(dp[N][0],dp[N][1]);
+    }
+};
+Input: 
+N = 4
+A[] = {1,-1,3,4}
+Output: 8
+Explanation: 
+We can choose 1st,3rd and 4th index,
+you can check that this is maximum
+possible sum. 
+
+Input: 
+N = 3
+arr[] =  {0,2,0};
+Output: 2
+Explanation: 
+We can choose 2nd index.You can 
+check sum is maximum in this case.
