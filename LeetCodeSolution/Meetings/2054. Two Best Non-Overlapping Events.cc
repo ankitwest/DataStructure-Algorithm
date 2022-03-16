67ed31c9af850adb78028ef65d0ba29a61bb5aec
Ankit Kumar <anshkrrr@gmail.com>
2022-03-17 01:45:26 +0530

    Update 2054. Two Best Non-Overlapping Events.cc

--- LeetCodeSolution/Meetings/2054. Two Best Non-Overlapping Events.cc
+++ LeetCodeSolution/Meetings/2054. Two Best Non-Overlapping Events.cc
@@ -1,30 +1,45 @@
 class Solution {
 public:
+    int nextEvent(vector<vector<int>>& events, int i){
+        int low = i+1; int high = events.size()-1;
+        int ans = events.size(); int val = events[i][1];
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(events[mid][0]>val)
+                ans = mid, high = mid-1;
+            else
+                low = mid+1;
+        }
+        return ans;
+    }
+    
     vector<vector<int>> dp;
-    int solve(vector<vector<int>>& events,int i,int eventsAttended){
-        if(i==events.size() or eventsAttended==2)
+    int solve(vector<vector<int>>& events,int i,int k){
+        if(i==events.size() or k==2)
             return 0;
         
-        if(dp[i][eventsAttended]!=-1)
-            return dp[i][eventsAttended];
+        if(dp[i][k]!=-1)
+            return dp[i][k];
         
-        vector<int> ans = {events[i][1],INT_MAX,INT_MAX};
-        int next = upper_bound(events.begin(),events.end(),ans) - events.begin();
+        int next = nextEvent(events,i);
         
         //pick  // not pick
-        int incl = events[i][2] + solve(events,next,eventsAttended+1);
-        int excl = solve(events,i+1,eventsAttended);
+        int incl = events[i][2] + solve(events,next,k+1);
+        int excl = solve(events,i+1,k);
         
-        return dp[i][eventsAttended] = max(incl,excl);
+        return dp[i][k] = max(incl,excl);
     }
     int maxTwoEvents(vector<vector<int>>& events) {
-        int n = events.size();
+        int n = events.size(); 
         dp.resize(n,vector<int>(2,-1));
         sort(events.begin(),events.end());
-        
         return solve(events,0,0);
     }
 };
+
+
+
+
 
 Input: events = [[1,3,2],[4,5,2],[2,4,3]]
 Output: 4
