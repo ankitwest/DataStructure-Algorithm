264c0093900e3d90ec72aaf023acd1235aaedcd5
Ankit Kumar <anshkrrr@gmail.com>
2022-03-26 22:56:32 +0530

    Create 47. Permutations II.cc

---
+++ LeetCodeSolution/Backtracking/47. Permutations II.cc
@@ -0,0 +1,28 @@
+
+// Pass by value only possible
+
+class Solution {
+public:
+    // in this pass by refernce gives wrong answer
+    vector<vector<int>> ans;
+    void helper(int idx,vector<int> nums,int n){
+        if(idx==n)
+            ans.push_back(nums);
+        
+        for(int i=idx;i<n;i++){
+            if(i!=idx and nums[i]==nums[idx]) continue;
+            swap(nums[i],nums[idx]);
+            helper(idx+1,nums,n);
+            // swap(nums[i],nums[idx]);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        helper(0,nums,n);
+        return ans;
+    }
+};
+
+// TC - n*n!
+// we can also use set , but TC will be (log n) extra
