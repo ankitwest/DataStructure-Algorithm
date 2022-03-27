35c58931a5ab3111e3a5eb6d6395efc2b92614ee
Ankit Kumar <anshkrrr@gmail.com>
2022-03-27 19:39:59 +0530

    Update 0046. Permutations.cc

--- LeetCodeSolution/Backtracking/0046. Permutations.cc
+++ LeetCodeSolution/Backtracking/0046. Permutations.cc
@@ -57,3 +57,34 @@ public:
 // No extra space is used  here
 
 // In approach 1 , we use a vector where we push and pop ; and vis array to keep track of element;
+
+
+
+
+
+
+
+
+
+// M-2
+
+class Solution {
+public:
+    // here back swap because we are making changes in the given array everytime we are calling helper function ;and we need to come back to zero;
+    vector<vector<int>> ans;
+    void helper(int idx,vector<int> &nums,int n){
+        if(idx==n)
+            ans.push_back(nums);
+        
+        for(int i=idx;i<n;i++){
+            temp.push_back(nums[i]);
+            helper(idx+1,nums,n);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
+        helper(0,nums,n);
+        return ans;
+    }
+};
