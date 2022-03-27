ccec72ce7276c2fda1fea459bdc4751e1eb39143
Ankit Kumar <anshkrrr@gmail.com>
2022-03-27 15:58:43 +0530

    Create 78. Subsets.cc

---
+++ LeetCodeSolution/Backtracking/78. Subsets.cc
@@ -0,0 +1,31 @@
+
+
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void helper(int idx,vector<int>& nums,vector<int>& subset){
+        ans.push_back(subset);
+           
+        for(int i=idx;i<nums.size();i++){
+            subset.push_back(nums[i]);
+            helper(i+1,nums,subset); // from where to start next one
+            subset.pop_back();
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> subset;
+        helper(0,nums,subset);
+        return ans;
+    }
+};
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
