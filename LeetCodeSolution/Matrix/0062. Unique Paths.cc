d6c1fe4ce3e43acc57dc74030b3d42ccdc79bac3
Ankit Kumar <anshkrrr@gmail.com>
2022-03-29 17:50:25 +0530

    Update 0062. Unique Paths.cc

--- LeetCodeSolution/Matrix/0062. Unique Paths.cc
+++ LeetCodeSolution/Matrix/0062. Unique Paths.cc
@@ -1,13 +1,34 @@
-
-   
 class Solution {
 public:
+      //DP(recursive+memoization)
+     int uniquePaths(int m, int n) {
+         vector<vector<int>> dp(m,vector<int>(n, -1));
+         auto solve = [&dp](auto solve,int i,int j){
+             if(i<0 or j<0)
+                 return 0;
+             if(i==0 or j==0)
+                 return 1;
+             if(dp[i][j]!=-1)
+                 return dp[i][j];
+            int ways = solve(solve,i-1,j) + solve(solve,i,j-1);
+             return dp[i][j] = ways;
+         };
+         return solve(solve,m-1,n-1);
+    }
+};
+
+class Solution2 {
+public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n, 1));
+        vector<vector<int>> dp(m,vector<int>(n, -1));
         
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j]=(dp[i-1][j]+dp[i][j-1]);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 and j==0) dp[i][j] = 1;
+                else if(i==0) dp[i][j] = 1;
+                else if(j==0) dp[i][j] = 1;
+                
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         return dp[m-1][n-1];
@@ -15,33 +36,10 @@ public:
 };
 
 
-M-2
-   //DP(recursive+memoization)
-    int solve(int i,int j,int m,int n,vector<vector<int>> &dp)
-    {
-        if(i>=m||j>=n)
-            return 0;
-        if(i==m-1&&j==n-1)
-            return 1;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        
-        return dp[i][j]=solve(i+1,j,m,n,dp)+solve(i,j+1,m,n,dp);
-    }
-    int uniquePaths(int m, int n) {
-      vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(0,0,m,n,dp);
-    }
-//TC:O(m*n)
-//SC:O(m*n)
 
 
 
-
-
-
-
-class Solution {
+class Solution3 {
 public:
     int uniquePaths(int m, int n) {
         vector<int> cur(n, 1);
@@ -49,13 +47,18 @@ public:
             for (int j = 1; j < n; j++) {
                 cur[j] += cur[j - 1];
 //                 cout<<cur[j]<<" ";
-                
             }
             cout<<endl;
         }
         return cur[n - 1];
     }
 };
+
+
+
+
+
+
 
 Input: m = 3, n = 2
 Output: 3
