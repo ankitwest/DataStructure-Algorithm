2e550a3b0277096286f328e052d0e8aa69ba839f
Ankit Kumar <anshkrrr@gmail.com>
2022-03-29 16:57:23 +0530

    Create largest-subtree-sum&-avg.cc

---
+++ GFG/BinaryTree/largest-subtree-sum&-avg.cc
@@ -0,0 +1,21 @@
+
+int findLargestSubtreeSumUtil(Node* root, int& ans)
+{
+    // If current node is null then
+    // return 0 to parent node.
+    if (root == NULL)    
+        return 0;
+     
+    // Subtree sum rooted at current node.
+    int currSum = root->key +
+      findLargestSubtreeSumUtil(root->left, ans)
+      + findLargestSubtreeSumUtil(root->right, ans);
+ 
+    // Update answer if current subtree
+    // sum is greater than answer so far.
+    ans = max(ans, currSum);
+ 
+    // Return current subtree sum to
+    // its parent node.
+    return currSum;
+}
