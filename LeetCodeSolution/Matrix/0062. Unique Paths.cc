82988a06a99806f7c36969d0622c0d9e5baf777d
Ankit Kumar <anshkrrr@gmail.com>
2022-03-29 20:02:54 +0530

    Update 0062. Unique Paths.cc

--- LeetCodeSolution/Matrix/0062. Unique Paths.cc
+++ LeetCodeSolution/Matrix/0062. Unique Paths.cc
@@ -1,15 +1,16 @@
+      //DP(recursive+memoization)
 class Solution {
 public:
-      //DP(recursive+memoization)
      int uniquePaths(int m, int n) {
-         vector<vector<int>> dp(m,vector<int>(n, -1));
+         vector<vector<int>> dp(m,vector<int>(n, 0));
          auto solve = [&dp](auto solve,int i,int j){
              if(i<0 or j<0)
                  return 0;
              if(i==0 or j==0)
                  return 1;
-             if(dp[i][j]!=-1)
+             if(dp[i][j]!=0)
                  return dp[i][j];
+             
             int ways = solve(solve,i-1,j) + solve(solve,i,j-1);
              return dp[i][j] = ways;
          };
@@ -17,18 +18,21 @@ public:
     }
 };
 
+
 class Solution2 {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n, -1));
-        
+        vector<vector<int>> dp(m,vector<int>(n, 0));
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 and j==0) dp[i][j] = 1;
-                else if(i==0) dp[i][j] = 1;
-                else if(j==0) dp[i][j] = 1;
-                
-                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+             dp[i][0]=1;
+        }
+        for(int j=0;j<n;j++){
+             dp[0][j]=1;
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         return dp[m-1][n-1];
