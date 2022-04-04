3be0ac3ff11e4be02172aef2e6cf5fe2ed39de6f
Ankit Kumar <anshkrrr@gmail.com>
2022-04-04 08:38:41 +0530

    Update next-greater-element.cc

--- GFG/Stack/next-greater-element.cc
+++ GFG/Stack/next-greater-element.cc
@@ -4,6 +4,8 @@ class Solution{
     vector<long long> nextLargerElement(vector<long long> arr, int n){
         vector<long long> v(n);
       stack<long long>st;
+      
+        // next greater <     next smaller >
       for(int i=n-1;i>=0;i--){
           while(!st.empty() and st.top()<arr[i])
             st.pop();
@@ -19,3 +21,32 @@ Input:
 N = 5, arr[] [6 8 0 1 3]
 Output:
 8 -1 1 3 -1
+
+  
+  
+  vector<long long> all4kind(vector<long long> arr, int n){
+        vector<long long> next(n);
+      stack<long long>st;
+      
+      // next greater <     next smaller >
+      for(int i=n-1;i>=0;i--){
+          while(!st.empty() and st.top()<arr[i])
+            st.pop();
+          if(!st.empty()) next[i]=st.top();
+          else next[i]=-1;
+          st.push(arr[i]);
+      }
+      return next;
+      
+      
+      vector<long long> prev(n);
+      // previous greater <     previous smaller >
+      for(int i=0;i<n;i++){
+          while(!st.empty() and st.top()>arr[i])
+            st.pop();
+          if(!st.empty()) prev[i]=st.top();
+          else prev[i]=-1;
+          st.push(arr[i]);
+      }
+      return prev;
+    }
