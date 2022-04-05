d0e5c359389ad5c7da43fde8ae09caff8b06f216
Ankit Kumar <anshkrrr@gmail.com>
2022-04-06 00:37:31 +0530

    Update Partition Equal Subset Sum.cc

--- GFG/DP/Partition Equal Subset Sum.cc
+++ GFG/DP/Partition Equal Subset Sum.cc
@@ -38,3 +38,37 @@ arr = {1, 5, 11, 5}
 Output: YES
 Explaination: 
 The two parts are {1, 5, 5} and {11}.
+
+    
+    
+    
+    
+    
+// Tabulation
+class Solution{
+public:
+     int equalPartition(int N, int arr[]){
+         int sum = accumulate(arr,arr+N,0);
+        if(sum&1) return false;
+        int n = N;
+        vector<vector<int>> dp(n+1,vector<int>(sum/2+1));
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+         for(int j=0;j<=sum/2;j++){
+             dp[0][j] = 0;
+         }
+         dp[0][0] = 1;
+         
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum/2;j++){
+                if(j>=arr[i-1])
+                dp[i][j] = dp[i-1][j] or dp[i-1][j-arr[i-1]];
+                else 
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][sum/2];
+     }
+};
