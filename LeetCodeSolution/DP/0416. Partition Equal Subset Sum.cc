572b7d655a2583db86ae38cb2a4a7b3ea945d491
Ankit Kumar <anshkrrr@gmail.com>
2022-04-07 18:58:12 +0530

    Create 0416. Partition Equal Subset Sum.cc

---
+++ LeetCodeSolution/DP/0416. Partition Equal Subset Sum.cc
@@ -0,0 +1,35 @@
+
+
+
+class Solution {
+public:
+    vector<vector<int>> dp; 
+    bool solve(int ind,int sum,vector<int>& nums){
+        if(sum==0)
+            return true;
+        if(sum<0)
+            return false;
+        if(sum>0 and ind>=nums.size())
+            return false;
+       
+        if(dp[ind][sum]!=-1)
+            return dp[ind][sum];
+        
+        bool a = solve(ind+1,sum-nums[ind],nums);
+        bool b = solve(ind+1,sum,nums);
+        
+        return dp[ind][sum] = (a or b);
+    }
+    bool canPartition(vector<int>& nums) {
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        if(sum&1) return false; 
+        int n = nums.size();
+        dp.resize(n+1,vector<int>(sum+1,-1));
+        
+        return solve(0,sum/2,nums);
+    }
+};
+
+
+
+// dp.resize(-1)  will only give right answer dp.resize(0) wwn't
