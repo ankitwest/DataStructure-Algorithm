6a2ac0d7fb59f3d8970bc2713bf04a15cf8eda32
Ankit Kumar <anshkrrr@gmail.com>
2022-04-16 03:06:46 +0530

    Update 0595. Big Countries.sql

--- LeetCodeSolution/SQL/0595. Big Countries.sql
+++ LeetCodeSolution/SQL/0595. Big Countries.sql
@@ -10,3 +10,9 @@ where area >= 3000000 or population >= 25000000;
 select product_id 
 from Products
 where low_fats = 'Y' and recyclable = 'Y'
+
+
+
+select name
+from Customer
+where referee_id != 2 or referee_id is null
