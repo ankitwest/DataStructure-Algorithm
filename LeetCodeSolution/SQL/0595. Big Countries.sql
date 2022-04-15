972b5300c272fd5cbfaa02c7783748a30a7bbf99
Ankit Kumar <anshkrrr@gmail.com>
2022-04-16 02:53:54 +0530

    Update and rename 0595. Big Countries.cc to 0595. Big Countries.sql

--- LeetCodeSolution/SQL/0595. Big Countries.sql
+++ LeetCodeSolution/SQL/0595. Big Countries.sql
@@ -2,3 +2,11 @@
 select name , area , population 
 from world
 where area >= 3000000 or population >= 25000000;
+
+
+
+
+
+select product_id 
+from Products
+where low_fats = 'Y' and recyclable = 'Y'
