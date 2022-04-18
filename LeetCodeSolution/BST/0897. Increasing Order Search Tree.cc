7f5091946af1b91e3093868d264136ae2b51380f
Ankit Kumar <anshkrrr@gmail.com>
2022-04-18 19:56:30 +0530

    Update 0897. Increasing Order Search Tree.cc

--- LeetCodeSolution/BST/0897. Increasing Order Search Tree.cc
+++ LeetCodeSolution/BST/0897. Increasing Order Search Tree.cc
@@ -29,5 +29,48 @@ public:
         
         solve(root,ans);
         return temp->right;
-    }a
+    }
+};
+
+
+
+//Behera
+class Solution {
+public:
+    TreeNode* increasingBST(TreeNode* root) {
+        if(root==nullptr) return nullptr;
+        
+        multiset<int> se;
+        
+        auto recur = [&se](TreeNode* node, const auto& recur){
+            if(node==nullptr) return;
+            
+            se.insert(node->val);
+            
+            recur(node->left, recur);
+            recur(node->right, recur);
+        };
+        
+        recur(root, recur);
+        
+        auto it = se.find(*se.begin());
+        
+        TreeNode* newRoot = new TreeNode(*it);
+        
+        TreeNode* tempNode = newRoot;
+
+        se.erase(it);
+
+        while(!se.empty()){
+            it = se.find(*se.begin());
+        
+            tempNode->right = new TreeNode(*it);
+        
+            tempNode = tempNode->right;
+            
+            se.erase(it);
+        }
+        
+        return newRoot;
+    }
 };
