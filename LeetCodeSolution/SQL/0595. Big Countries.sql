27f202a582560be7db4d0aa0156df2095c7f5cdb
Ankit Kumar <anshkrrr@gmail.com>
2022-04-22 16:25:11 +0530

    Update 0595. Big Countries.sql

--- LeetCodeSolution/SQL/0595. Big Countries.sql
+++ LeetCodeSolution/SQL/0595. Big Countries.sql
@@ -51,3 +51,20 @@ SET N = N-1;
         FROM Employee ORDER BY salary DESC LIMIT 1 OFFSET N
   );
 END
+
+
+
+
+https://leetcode.com/problems/employees-with-missing-information/
+select e.employee_id
+from Employees e left join Salaries s on e.employee_id = s.employee_id
+where s.salary is null
+
+union
+
+select s.employee_id
+from Salaries s left join Employees e on s.employee_id = e.employee_id
+where e.name is null
+
+order by employee_id
+
