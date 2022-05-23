bd626a6c2a220d5e553ea8ca7ac3e351fc813348
Ankit Kumar <anshkrrr@gmail.com>
2022-05-24 02:02:11 +0530

    Update #max-rectangular-area-in-a-histogram&Max-rectangular-area-in-a-matrix.cc

--- GFG/Stack/#max-rectangular-area-in-a-histogram&Max-rectangular-area-in-a-matrix.cc
+++ GFG/Stack/#max-rectangular-area-in-a-histogram&Max-rectangular-area-in-a-matrix.cc
@@ -63,42 +63,42 @@ public:
 // with respect to i ;
 //  confusing
 
-class Solution
-{
-    public:
-    //Function to find largest rectangular area possible in a given histogram.
-    long long getMaxArea(long long arr[], int n)
-    {
-        stack<int> st;
-        vector<int> left(n),right(n);
+// class Solution
+// {
+//     public:
+//     //Function to find largest rectangular area possible in a given histogram.
+//     long long getMaxArea(long long arr[], int n)
+//     {
+//         stack<int> st;
+//         vector<int> left(n),right(n);
         
-        for(int i=0;i<n;i++){
-            while(!st.empty() and arr[st.top()]>=arr[i]){
-                st.pop();
-            }
-            if(st.empty()) left[i]=i+1;
-            else left[i]=i-st.top();
-            st.push(i);
-        }
+//         for(int i=0;i<n;i++){
+//             while(!st.empty() and arr[st.top()]>=arr[i]){
+//                 st.pop();
+//             }
+//             if(st.empty()) left[i]=i+1;
+//             else left[i]=i-st.top();
+//             st.push(i);
+//         }
         
-        while(!st.empty()) st.pop();
+//         while(!st.empty()) st.pop();
         
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() and arr[st.top()]>=arr[i]){
-                st.pop();
-            }
-            if(st.empty()) right[i]=n-i;
-            else right[i]=st.top()-i;
-            st.push(i);
-        }
+//         for(int i=n-1;i>=0;i--){
+//             while(!st.empty() and arr[st.top()]>=arr[i]){
+//                 st.pop();
+//             }
+//             if(st.empty()) right[i]=n-i;
+//             else right[i]=st.top()-i;
+//             st.push(i);
+//         }
         
-         long long max_area=0;
-       for(int i=0;i<n;i++){
-           max_area=max(max_area,arr[i]*(right[i]+left[i]-1));
-       }
-       return max_area; 
-    }
-};
+//          long long max_area=0;
+//        for(int i=0;i<n;i++){
+//            max_area=max(max_area,arr[i]*(right[i]+left[i]-1));
+//        }
+//        return max_area; 
+//     }
+// };
 
 
 
@@ -111,6 +111,8 @@ class Solution
 
 
 // Max rectangular area in a matrix
+// question matches with Maximum stregth of wizards
+// https://leetcode.com/problems/sum-of-total-strength-of-wizards/
 
 class Solution{
   public:
@@ -161,3 +163,60 @@ class Solution{
         return ans;
     }
 };
+
+
+
+// Subarray problem
+// https://leetcode.com/problems/sum-of-total-strength-of-wizards/
+class Solution {
+public:
+    const long long mod = 1e9+7;
+    int totalStrength(vector<int>& arr) {
+        int n = arr.size(); 
+        vector<long long> preSum(n+1,0LL);
+        vector<long long> prePrefix(n+2,0LL);
+        
+        for(int i=0;i<n;i++){    // sum of first k elements
+            preSum[i+1] = (preSum[i] + arr[i])%mod;
+        }
+        for(int i=0;i<=n;i++){   // sum of first k prefix
+            prePrefix[i+1] = (prePrefix[i] + preSum[i])%mod;
+        }
+        
+        vector<long long> left(n,0),right(n,0);
+        stack<long long> st;
+        
+        // first index on left < current value
+        for(int i=0;i<n;i++){
+            while(!st.empty() and arr[st.top()] >= arr[i])
+                st.pop();
+            left[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+        
+        // first index on right <= current value
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() and arr[st.top()] > arr[i])
+                st.pop();
+            right[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        // positive parts:
+        // (prefix[i + 1] + prefix[i + 2] + ... + prefix[right]) * (i - left) = (prePrefix[right[i] + 1] - prePrefix[i + 1]) * (i - left[i])
+        // negative parts:
+        // (prefix[left + 1] + prefix[left + 2] + ... + prefix[i]) * (right - i) = (prePrefix[i + 1] - prePrefix[left[i] + 1]) * (right[i] - i)
+    
+        long long ans = 0;
+        for(int i=0;i<n;i++){
+            long long L = left[i]; long long R = right[i];
+            long long pos = ((prePrefix[R + 1] - prePrefix[i + 1]) * (i - L)) % mod;
+            long long neg = ((prePrefix[i + 1] - prePrefix[L + 1]) * (R - i)) % mod;
+            ans += ((pos - neg) % mod + mod) * arr[i] % mod;
+            ans %= mod;
+        }
+        return ans;
+    }
+};
+
+
