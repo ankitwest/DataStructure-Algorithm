ac4e597d3ebddb7ae1b4b6b5ae87c68ccc02ff09
Ankit Kumar <anshkrrr@gmail.com>
2022-05-24 02:30:57 +0530

    Update 0907. Sum of Subarray Minimums.cc

--- LeetCodeSolution/Stack/0907. Sum of Subarray Minimums.cc
+++ LeetCodeSolution/Stack/0907. Sum of Subarray Minimums.cc
@@ -41,3 +41,10 @@ public:  //better approach // first smaller element ; then size of subarray ; th
         return sum%mod;
     }
 };
+
+Input: arr = [3,1,2,4]
+Output: 17
+Explanation: 
+Subarrays are [3], [1], [2], [4], [3,1], [1,2], [2,4], [3,1,2], [1,2,4], [3,1,2,4]. 
+Minimums are 3, 1, 2, 4, 1, 1, 2, 1, 1, 1.
+Sum is 17.
