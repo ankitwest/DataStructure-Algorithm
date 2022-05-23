2917bf4904fb31134d38e1088e611727f6f9f84e
Ankit Kumar <anshkrrr@gmail.com>
2022-05-23 23:44:30 +0530

    Update topological-sorting.cc

--- GFG/Graph/topological-sorting.cc
+++ GFG/Graph/topological-sorting.cc
@@ -30,3 +30,37 @@ public:
 
 Expected Time Complexity: O(V + E).
 Expected Auxiliary Space: O(V).
+
+	
+	//BFS    kahn's algo
+	
+	class Solution
+	{
+  public:
+    vector<int> findOrder(int n, int m, vector<vector<int>> pre) 
+    {
+        vector<vector<int>> adj(n);
+        vector<int> indeg(n,0);
+        for(int i=0;i<pre.size();i++){
+            adj[pre[i][1]].push_back(pre[i][0]);
+            indeg[pre[i][0]]++;
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0) q.push(i);
+        }
+        
+        vector<int> toposort;
+        while(!q.empty()){
+            auto curr = q.front();q.pop();
+            toposort.push_back(curr);
+            for(auto neigh:adj[curr]){
+                indeg[neigh]--;
+                if(indeg[neigh]==0) q.push(neigh);
+            }
+        }
+        
+        if(toposort.size()==n) return toposort;
+        return {};
+    }
+	};
