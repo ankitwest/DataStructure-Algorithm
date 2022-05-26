60a036447b0321986316ea3c4a6881b7846eea64
Ankit Kumar <anshkrrr@gmail.com>
2022-05-26 22:15:51 +0530

    Update #Moore Voting Algorithm-Majority Element.cc

--- GFG/Array/#Moore Voting Algorithm-Majority Element.cc
+++ GFG/Array/#Moore Voting Algorithm-Majority Element.cc
@@ -48,3 +48,56 @@ Explanation:
 Since, 3 is present more
 than N/2 times, so it is 
 the majority element.
+  
+  
+  
+  
+//  Q. find element greater than n/3
+  
+  #include <bits/stdc++.h>
+
+using namespace std;
+vector < int > majorityElement(int nums[], int n) {
+  int sz = n;
+  int num1 = -1, num2 = -1, count1 = 0, count2 = 0, i;
+  for (i = 0; i < sz; i++) {
+    if (nums[i] == num1)
+      count1++;
+    else if (nums[i] == num2)
+      count2++;
+    else if (count1 == 0) {
+      num1 = nums[i];
+      count1 = 1;
+    } else if (count2 == 0) {
+      num2 = nums[i];
+      count2 = 1;
+    } else {
+      count1--;
+      count2--;
+    }
+  }
+  vector < int > ans;
+  count1 = count2 = 0;
+  for (i = 0; i < sz; i++) {
+    if (nums[i] == num1)
+      count1++;
+    else if (nums[i] == num2)
+      count2++;
+  }
+  if (count1 > sz / 3)
+    ans.push_back(num1);
+  if (count2 > sz / 3)
+    ans.push_back(num2);
+  return ans;
+}
+
+int main() {
+  int arr[] = {1,2,2,3,2};
+  vector < int > majority;
+  majority = majorityElement(arr, 5);
+  cout << "The majority element is ";
+
+  for (auto it: majority) {
+    cout << it << " ";
+  }
+}
