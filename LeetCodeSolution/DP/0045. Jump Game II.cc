5af3b7cb018b9d978ad49f490b29023241ce7040
Ankit Kumar <anshkrrr@gmail.com>
2022-05-28 18:31:28 +0530

    Update 0045. Jump Game II.cc

--- LeetCodeSolution/DP/0045. Jump Game II.cc
+++ LeetCodeSolution/DP/0045. Jump Game II.cc
@@ -1,3 +1,29 @@
+//better
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int maxReachable = 0; int jump=0; int curr=0;
+        int n=nums.size();
+        
+        for(int i=0;i<n;i++){
+            if(maxReachable<i) return -1;
+            maxReachable = max(maxReachable,nums[i]+i);
+            
+            if(curr==i){
+                if(i==n-1) continue;
+                jump++;
+                curr=maxReachable;
+            }
+        }
+        return jump;
+    }
+};
+
+
+
+
+// M-2
 class Solution {
 public:
     int jump(vector<int>& nums) {
