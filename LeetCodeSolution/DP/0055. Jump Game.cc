1d00fcaf62b70eb5551ef54bff189104599dc582
Ankit Kumar <anshkrrr@gmail.com>
2022-05-28 18:27:29 +0530

    Update 0055. Jump Game.cc

--- LeetCodeSolution/DP/0055. Jump Game.cc
+++ LeetCodeSolution/DP/0055. Jump Game.cc
@@ -1,8 +1,27 @@
+
+class Solution {
+public:
+     bool canJump(vector<int>& nums) {
+         int n = nums.size();
+         int maxReachable = 0; int curr=0;
+         
+       for(int i=0;i<n;i++){
+           if(maxReachable<i) return false;
+           maxReachable = max(maxReachable,nums[i]+i);
+       }
+         return true;
+    }
+};
+
+
+
+// M-2  recursion
+
 class Solution {
 public:
     bool jump(vector<int>& nums,int l) {
-        
         if(l==0) return true;
+        
         for(int i=l-1;i>=0;i--){
             if(nums[i]>=l-i)
                 return jump(nums,i);
