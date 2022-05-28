1c60534cb46e3b0f978bad65e99243eb06c81bca
Ankit Kumar <anshkrrr@gmail.com>
2022-05-28 15:20:49 +0530

    Update 0160. Intersection of Two Linked Lists.cc

--- LeetCodeSolution/LL/0160. Intersection of Two Linked Lists.cc
+++ LeetCodeSolution/LL/0160. Intersection of Two Linked Lists.cc
@@ -1,43 +1,28 @@
 M-1
 
-int findlen(ListNode* head){
-        ListNode* temp = head;
-        int l = 0;
-        while(temp!=NULL){
-            l++;
-            temp = temp->next;
-        }
-        return l;
-    }
+class Solution2 {
+public:
     ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
-        int l1 = findlen(head1);
-        int l2 = findlen(head2);
+        int l1 = 0; int l2 = 0;
+        ListNode* a = head1; ListNode* b = head2;
         
-         ListNode* temp1 = head1;
-         ListNode* temp2 = head2;
+        if(!a or !b) return NULL;
         
-        int extra=0;
-        if(l2>l1){
-            extra = l2-l1;
-            while(extra--){
-                temp2=temp2->next;
-            }
-        }else{
-            extra = l1-l2;
-            while(extra--){
-                temp1=temp1->next;
-            }
+        while(a) l1++,a=a->next;
+        while(b) l2++,b=b->next;
+        
+        if(l2>l1) swap(head1,head2),swap(l1,l2);
+        
+        int l = l1-l2;
+        while(l--) head1=head1->next;
+        
+        while(head1!=head2 and head1 and head2){
+            head1=head1->next;head2=head2->next;
         }
-        while(temp1!=temp2 and temp1!=NULL and temp2!=NULL){
-            temp1=temp1->next;
-            temp2=temp2->next;
-        }
-        return temp1;
-        
-        
-
+        return head1;
     }
-
+};
+        
 
 
 M-2
