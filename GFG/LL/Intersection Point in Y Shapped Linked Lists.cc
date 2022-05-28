13e0a232cfab37c77e2efcfdd34b55a7fa827336
Ankit Kumar <anshkrrr@gmail.com>
2022-05-28 15:23:14 +0530

    Update Intersection Point in Y Shapped Linked Lists.cc

--- GFG/LL/Intersection Point in Y Shapped Linked Lists.cc
+++ GFG/LL/Intersection Point in Y Shapped Linked Lists.cc
@@ -1,26 +1,48 @@
 //Intersection Point in Y Shapped Linked Lists
 
-
-int intersectPoint(Node* head1, Node* head2)
+int intersectionPoint2(Node* head1, Node* head2)
 {
-    Node* h1 = head1;
-    Node* h2 = head2;
-    if(!h1 and !h2)
-        return -1;
-    while(h1 and h2 and h1!=h2){
-        h1=h1->next;
-        h2=h2->next;
+    Node* a = head1;
+    Node* b = head2;
+    int l1=0,l2=0;
+    while(a) l1++,a=a->next;
+    while(b) l2++,b=b->next;
+    
+    if(l2>l1) swap(head1,head2),swap(l1,l2);
+    
+    // if(l1>l2){
+        int l = l1-l2;
+        while(l--) head1=head1->next;
         
-        if(h1==h2)
-            return h1->data;
-        if(!h1)
-            h1=head2;
-        if(!h2)
-            h2=head1;
-    }
-    return -1;
+        while(head1!=head2){
+            head1=head1->next;head2=head2->next;
+        }
+        return head1->data;
+    // } 
 }
-
+int intersectionPoint(Node* head1, Node* head2)
+{
+    Node* a = head1;
+    Node* b = head2;
+    if(!a or !b) return -1;
+    
+    // while(a and b and a!=b){
+    //     a = a->next;
+    //     b = b->next;
+        
+    //     if(a==b) return a->data;
+        
+    //     if(!a) a = head2;
+    //     if(!b) b = head1;
+    // }
+    
+    // return -1;
+    while(a!=b){
+        a = (a == NULL) ? head2 : a->next;
+        b = (b == NULL) ? head1 : b->next;
+    }
+    return a->data;
+}
 Input:
 LinkList1 = 3->6->9->common
 LinkList2 = 10->common
@@ -28,48 +50,3 @@ common = 15->30->NULL
 Output: 15
 Explanation:
 
-
-
-class Solution
-{
-public:
-    int cut(Node* a,Node* b,int d){
-        while(d--){
-            // if(a==NULL)
-            //     return -1;
-            a=a->next;
-        }
-        while(a and b){
-            if(a==b)
-                return a->data;
-            a=a->next;
-            b=b->next;
-        }
-    }
-    
-    int intersectPoint(Node* head1, Node* head2)
-    {
-        //
-        auto count = [&](Node* head) -> int{
-           int l = 0;
-           while(head){
-               l++;
-               head = head->next;
-           }
-           return l;
-       };
-       
-       int l1 = count(head1);
-       int l2 = count(head2);
-       
-       int meet=0;
-       if(l1>l2){
-           int d = l1-l2;
-           meet = cut(head1,head2,d);
-       }else{
-           int d = l2-l1;
-           meet = cut(head2,head1,d);
-       }
-       return meet;
-    }
-};
