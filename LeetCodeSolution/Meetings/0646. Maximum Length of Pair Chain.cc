75fd917915be2975dea4a685254d4997e456be83
Ankit Kumar <anshkrrr@gmail.com>
2022-05-30 21:20:27 +0530

    Create 0646. Maximum Length of Pair Chain.cc

---
+++ LeetCodeSolution/Meetings/0646. Maximum Length of Pair Chain.cc
@@ -0,0 +1,44 @@
+
+
+class Solution {
+public:  //greedy
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(),pairs.end(),[&](vector<int>&a ,vector<int>&b){
+            if(a[1]==b[1]) return a[0]<b[0];
+            return a[1]<b[1];
+        });
+        
+        int right = pairs[0][1]; int len = 1;
+        for(int i=1;i<pairs.size();i++){
+            if(right < pairs[i][0]){
+                len++;
+                right = pairs[i][1];
+            }
+        }
+        return len;
+    }
+};
+
+
+
+// dp
+
+class Solution2 {
+public:  //lis  dp
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(),pairs.end());
+        int n = pairs.size();
+        vector<int> dp(n,1);
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(pairs[j][1] < pairs[i][0] and dp[i] < dp[j] + 1){
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+};
+
+A pair p2 = [c, d] follows a pair p1 = [a, b] if b < c. A chain of pairs can be formed in this fashion.
