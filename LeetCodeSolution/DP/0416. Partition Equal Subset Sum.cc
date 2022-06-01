f07e0d1a5087161d282a45ce2922b0342cf346aa
Ankit Kumar <anshkrrr@gmail.com>
2022-06-01 23:17:39 +0530

    Update 0416. Partition Equal Subset Sum.cc

--- LeetCodeSolution/DP/0416. Partition Equal Subset Sum.cc
+++ LeetCodeSolution/DP/0416. Partition Equal Subset Sum.cc
@@ -33,3 +33,43 @@ public:
 
 
 // dp.resize(-1)  will only give right answer dp.resize(0) wwn't
+
+
+
+
+
+
+
+// Tabulation
+class Solution{
+public:
+     bool canPartition(vector<int>& arr){
+         
+         int sum = accumulate(arr.begin(),arr.end(),0);
+        if(sum&1) return false;
+        int n = arr.size();
+        vector<vector<int>> dp(n+1,vector<int>(sum/2+1));
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+         for(int j=0;j<=sum/2;j++){
+             dp[0][j] = 0;
+         }
+         dp[0][0] = 1;
+        
+       bool take = false;
+	   bool notTake = false;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum/2;j++){
+                 if(j>=arr[i-1]) {
+	                take = dp[i-1][j-arr[i-1]];
+	            }
+	                
+	           notTake = dp[i-1][j];
+	           dp[i][j] = take or notTake;
+            }
+        }
+        return dp[n][sum/2];
+     }
+};
