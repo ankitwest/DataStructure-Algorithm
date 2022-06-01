ab0202b2428c4c9b03589314d5a521b74b96f5ae
Ankit Kumar <anshkrrr@gmail.com>
2022-06-01 18:29:34 +0530

    Update 1818. Minimum Absolute Sum Difference.cc

--- LeetCodeSolution/BS/1818. Minimum Absolute Sum Difference.cc
+++ LeetCodeSolution/BS/1818. Minimum Absolute Sum Difference.cc
@@ -20,3 +20,11 @@ public:
         return (total - max_decrement) % mod;
     }
 };
+
+
+Input: nums1 = [1,7,5], nums2 = [2,3,5]
+Output: 3
+Explanation: There are two possible optimal solutions:
+- Replace the second element with the first: [1,7,5] => [1,1,5], or
+- Replace the second element with the third: [1,7,5] => [1,5,5].
+Both will yield an absolute sum difference of |1-2| + (|1-3| or |5-3|) + |5-5| = 3.
