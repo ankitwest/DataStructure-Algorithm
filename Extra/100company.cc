e82c5259159bde9404a682d821879344241cc205
Ankit Kumar <anshkrrr@gmail.com>
2022-06-04 16:14:06 +0530

    Create 100company.cc

---
+++ Extra/100company.cc
@@ -0,0 +1,101 @@
+
+Adobe
+Airbnb
+Alphonso
+Amazon
+Apna
+Apple
+Arcesium
+Atlassian
+Blackbuck
+Booking(dot) com
+Browserstack
+Codenation
+Cohesity
+CoinDCX
+Coinbase
+Coinswitch Kuber
+Compass
+Confluent
+Cred
+Cure fit
+DE Shaw
+Discord
+Doordash
+Dunzo
+Eightfold(dot) ai
+Expedia
+Flipkart
+Freshworks
+Gojek
+Goldman Sachs
+Google
+Grab
+Graviton
+Hotstar
+Indeed
+Intuit
+JP Morgan Chase
+Khatabook
+Licious
+LinkedIn
+MakeMyTrip
+Media(dot) net
+Meesho
+Meta
+Microsoft
+Mindtickle
+Mobikwik
+Morgan Stanley
+Myntra
+Navi
+Netflix
+Ninjacart
+Notion
+Nutanix
+Nvidia
+Observe(dot) ai
+Ola
+Oracle (OCI)
+Oyo
+Palantir
+Palo Alto Networks
+PayPal
+Phonepe
+Pinterest
+Plaid
+Postman
+Prophecy(dot) io
+Rakuten
+Razorpay
+Reddit
+Redhat
+Rippling
+Rivigo
+Roblox
+Rubrik
+Rupeek
+Rupifi
+Salesforce
+Samsung
+ServiceNow
+Sharechat
+Slack
+Splunk
+Sprinklr
+Stripe
+Swiggy
+Tekion
+TikTok
+Tower Research
+Twilio
+Twitter
+Uber
+Udaan
+Unacademy
+Upstox
+VMware
+Walmart
+Zeta
+Zomato
+Zscaler
