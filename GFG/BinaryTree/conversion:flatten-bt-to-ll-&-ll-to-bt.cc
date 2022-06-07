08ccb96ed1c3e208dc23a523d7d461b6b99aa339
Ankit Kumar <anshkrrr@gmail.com>
2022-06-07 19:15:57 +0530

    Create conversion:flatten-bt-to-ll-&-ll-to-bt.cc

---
+++ GFG/BinaryTree/conversion:flatten-bt-to-ll-&-ll-to-bt.cc
@@ -0,0 +1,90 @@
+// Flatten binary tree to linked list
+class Solution
+{
+    public:
+    void flatten(Node *root)
+    {
+        if(!root or !root->left and !root->right)
+            return ;
+        if(root->left){
+            flatten(root->left);
+            Node* tempRight = root->right;
+            root->right = root->left;
+            root->left = NULL;
+            
+            Node* t = root->right;
+            while(t->right){
+                t = t->right;
+            }
+            t->right = tempRight;
+        }
+        flatten(root->right);
+    }
+};
+Input : 
+          1
+        /   \
+       2     5
+      / \     \
+     3   4     6
+
+Output :
+1 2 3 4 5 6 
+Explanation: 
+After flattening, the tree looks 
+like this
+    1
+     \
+      2
+       \
+        3
+         \
+          4
+           \
+            5
+             \
+              6 
+Here, left of each node points 
+to NULL and right contains the 
+next node in preorder.The inorder 
+traversal of this flattened tree 
+is 1 2 3 4 5 6.
+
+
+
+
+
+  
+  
+ 
+  //Function to make binary tree from linked list.
+  void convert(Node *head, TreeNode *&root) {
+    queue<TreeNode*>q;
+    root = new TreeNode(head->data);
+    q.push(root);
+    
+    while(!q.empty()){
+        TreeNode* curr = q.front();q.pop();
+        if(head->next){
+            curr->left = new TreeNode(head->next->data);
+            q.push(curr->left);
+            head = head->next;
+        }else if(head->next){
+            curr->right = new TreeNode(head->next->data);
+            q.push(curr->right);
+            head = head->next;
+        }
+    }
+}
+  
+  N = 5
+K = 5->4->3->2->1
+Output: 5 4 3 2 1
+  
+     5
+   /  \
+  4    3
+ / \
+2    1
+Now, the level order traversal of
+the above tree is 5 4 3 2 1.
