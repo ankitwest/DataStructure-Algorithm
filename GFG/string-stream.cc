02bdcbfa4b6399ef65599051dd942a47a11e781f
Ankit Kumar <anshkrrr@gmail.com>
2022-06-09 02:22:01 +0530

    Update string-stream.cc

--- GFG/string-stream.cc
+++ GFG/string-stream.cc
@@ -28,3 +28,24 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        unordered_map<string,int> m;
+        vector<string> ans;
+        stringstream a(s1); stringstream b(s2); string word;
+        while(a >> word)
+            m[word]++;
+
+        while(b >> word)
+            m[word]++;
+
+        for(auto &x :m)
+            if(x.second==1) ans.push_back(x.first);
+
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
