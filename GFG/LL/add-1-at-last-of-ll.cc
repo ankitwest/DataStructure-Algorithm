c223add239b833361f6529c8d845c24f46e77df7
Ankit Kumar <anshkrrr@gmail.com>
2022-06-09 20:48:28 +0530

    Update add-1-at-last-of-ll.cc

--- GFG/LL/add-1-at-last-of-ll.cc
+++ GFG/LL/add-1-at-last-of-ll.cc
@@ -17,26 +17,48 @@ class Solution
         head = reverse(head);
         bool f = true;
         Node* curr = head;
-        while(curr!=NULL and f==true){
-            if(curr->next==NULL and curr->data==9){
-                Node* newNode = new Node(1);
-                curr->next = newNode;
-                curr->data = 0;
-                f = false;
-            }else if(curr->next!=NULL and curr->data==9 and curr->next->data<9){
-                curr->next->data += 1;
-                curr->data = 0;
-                f= false;
-            }else if(curr->next!=NULL and curr->data==9 and curr->next->data==9){
-                curr->data = 0;
-                curr=curr->next;
-            }else{
-                curr->data += 1;
-                 f= false;
+        
+        while(curr){
+            curr->data = (curr->data+1)%10;
+            
+            if(curr->data>=1 and curr->data<=9) 
+                break; 
+                
+            if(curr->next)    
+                curr = curr->next;
+            else{
+                curr->next = new Node(1);
+                break;
             }
         }
+        
         return reverse(head);
     }
+//     Node* addOne(Node *head) 
+//     {
+//         head = reverse(head);
+//         bool f = true;
+//         Node* curr = head;
+//         while(curr!=NULL and f==true){
+//             if(curr->next==NULL and curr->data==9){
+//                 Node* newNode = new Node(1);
+//                 curr->next = newNode;
+//                 curr->data = 0;
+//                 f = false;
+//             }else if(curr->next!=NULL and curr->data==9 and curr->next->data<9){
+//                 curr->next->data += 1;
+//                 curr->data = 0;
+//                 f= false;
+//             }else if(curr->next!=NULL and curr->data==9 and curr->next->data==9){
+//                 curr->data = 0;
+//                 curr=curr->next;
+//             }else{
+//                 curr->data += 1;
+//                  f= false;
+//             }
+//         }
+//         return reverse(head);
+//     }
 };
 
 LinkedList: 1->2->3
