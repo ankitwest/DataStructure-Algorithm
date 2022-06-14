668b34d65ad435221837bc16a5680d14e7ce4b75
Ankit Kumar <anshkrrr@gmail.com>
2022-06-14 14:38:13 +0530

    Create 0209. Minimum Size Subarray Sum.cc

---
+++ LeetCodeSolution/Array/Subarray/0209. Minimum Size Subarray Sum.cc
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool check(int len,int target, vector<int>& nums){
+        int sum = 0; int n = nums.size(); 
+        for(int i=0;i<len;i++){
+            sum += nums[i]; 
+        }
+        if(sum>=target) return true;
+        for(int i=len;i<n;i++){
+            sum = sum + nums[i] - nums[i-len];
+            if(sum>=target) return true;
+        }
+        return false;
+    }
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+         int l=0,r=n;
+        int ans = 0;
+        while(l<=r){
+            int mid = (l+r)>>1;
+            if(check(mid,target,nums))
+                ans = mid, r = mid-1;
+            else
+                l = mid+1;
+        }
+        return ans;
+    }
+};
+
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
