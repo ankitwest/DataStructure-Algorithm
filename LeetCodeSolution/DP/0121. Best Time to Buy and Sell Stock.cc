b2c7c2b6abcc547a1873c03dbd86fd6c0984ba60
Ankit Kumar <anshkrrr@gmail.com>
2022-06-17 03:10:13 +0530

    Create 0121. Best Time to Buy and Sell Stock.cc

---
+++ LeetCodeSolution/DP/0121. Best Time to Buy and Sell Stock.cc
@@ -0,0 +1,137 @@
+class Solution {
+public:
+    int f(int ind,int buy,int k,vector<int>& prices,vector<vector<int>>& dp){
+        if(ind>=prices.size())
+            return 0;
+        if(k<=0)
+            return 0;
+        
+        if(dp[ind][buy]!=-1)
+            return dp[ind][buy];
+        
+        int profit = 0;
+        if(buy)
+            profit = max(-prices[ind] + f(ind+1,!buy,k,prices,dp), 0 + f(ind+1,buy,k,prices,dp) );
+        else
+            profit = max(+prices[ind] + f(ind+1,!buy,k-1,prices,dp), 0 + f(ind+1,buy,k,prices,dp) );
+        
+        return dp[ind][buy] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int ind = 0; int buy = 1; int k = 1;
+        vector<vector<int>> dp(n+1,vector<int> (2,-1));
+        return f(ind,buy,k,prices,dp);
+    }
+};
+
+//tabulation
+class Solution2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int ind = 0; int buy = 1; int k = 1;
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1,0)));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<2;buy++){
+                for(int cap=1;cap<=k;cap++){
+                    int profit = 0;
+                    if(buy)
+                        profit = max(-prices[ind] + dp[ind+1][!buy][cap], 0 + dp[ind+1][buy][cap] );
+                    else
+                        profit = max(+prices[ind] + dp[ind+1][!buy][cap-1], 0 + dp[ind+1][buy][cap] );
+
+                     dp[ind][buy][cap] = profit;
+                }
+            }
+        }
+        return dp[0][1][k];
+    }
+};
+
+// further space optimization
+class Solution3 {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int ind = 0; int buy = 1; int k = 1;
+        vector<vector<int>> curr(2, vector<int>(k+1,0)); 
+        vector<vector<int>> after(2, vector<int>(k+1,0));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<2;buy++){
+                for(int cap=1;cap<=k;cap++){
+                    int profit = 0;
+                    if(buy)
+                        profit = max(-prices[ind] + after[!buy][cap]    , 0 + after[buy][cap] );
+                    else
+                        profit = max(+prices[ind] + after[!buy][cap-1]  , 0 + after[buy][cap] );
+
+                    curr[buy][cap] = profit;
+                }
+                after = curr;
+            }
+        }
+        return curr[1][k];
+    }
+};
+
+
+
+class Solution10 {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price = prices[0];
+        int curr = 0; int max_profit = 0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i] - min_price > max_profit)
+                max_profit = prices[i]-min_price;
+            if(prices[i] < min_price) 
+                min_price = prices[i];
+        }
+        return max_profit;
+    }
+};
+
+
+
+// similar
+class Solution11 {
+public:
+    int maxProfit(vector<int>& prices){
+        int n = prices.size();
+        int profit = 0;
+        int minPrice = INT_MAX;
+        for(int i=0;i<n;i++){
+            minPrice = min(minPrice,prices[i]);
+            profit = max(profit,prices[i]-minPrice);
+        }
+        return profit;
+    }
+};
+
+
+
+// // not good
+// class Solution4 {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         if(n<2) return 0;
+//         int minPrice = prices[0];
+//         int profit=0;
+//         for(int i=1;i<n;i++){
+//             if(prices[i]>prices[i-1])
+//                 profit = max(profit,prices[i]-minPrice);
+//             else
+//                 minPrice = min(minPrice , prices[i]);
+//         }
+//         return profit;
+//     }
+// };
+
+
+
+
+
