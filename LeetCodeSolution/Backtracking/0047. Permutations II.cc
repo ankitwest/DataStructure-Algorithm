fdaaeb68d427395a10b100b13d46e3c215d1f9ae
Ankit Kumar <anshkrrr@gmail.com>
2022-06-18 22:05:18 +0530

    Update 0047. Permutations II.cc

--- LeetCodeSolution/Backtracking/0047. Permutations II.cc
+++ LeetCodeSolution/Backtracking/0047. Permutations II.cc
@@ -1,34 +1,5 @@
 
 // Pass by value only possible
-
-class Solution {
-public:
-    // in this pass by refernce gives wrong answer
-    vector<vector<int>> ans;
-    void helper(int idx,vector<int> nums,int n){
-        if(idx==n)
-            ans.push_back(nums);
-        
-        for(int i=idx;i<n;i++){
-            if(i!=idx and nums[i]==nums[idx]) continue;
-            swap(nums[i],nums[idx]);
-            helper(idx+1,nums,n);
-            // swap(nums[i],nums[idx]);
-        }
-    }
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        helper(0,nums,n);
-        return ans;
-    }
-};
-
-// TC - n*n!
-// we can also use set , but TC will be (log n) extra
-
-
-
 //latest
 class Solution {
 public:
@@ -41,7 +12,6 @@ public:
         }
         
         for(int i=idx;i<n;i++){
-            // if(i>idx and nums[i]==nums[idx]) continue;
             if(i==idx || nums[i]!=nums[idx]){ 
                 swap(nums[i],nums[idx]);
                 helper(idx+1,nums,n);
@@ -56,4 +26,13 @@ public:
         return ans;
     }
 };
+//             if(i!=idx and nums[i]==nums[idx]) continue;
+
+
+
+// TC - n*n!
+// we can also use set , but TC will be (log n) extra
+
+
+
 
