bc4d77d0292d873a809f2d44fa15a57158750ce3
Ankit Kumar <anshkrrr@gmail.com>
2022-06-18 21:31:22 +0530

    Update 1247. Minimum Swaps to Make Strings Equal.cc

--- LeetCodeSolution/String/1247. Minimum Swaps to Make Strings Equal.cc
+++ LeetCodeSolution/String/1247. Minimum Swaps to Make Strings Equal.cc
@@ -3,15 +3,17 @@ public:
     int minimumSwap(string s1, string s2) {
         int xy=0;int yx=0;
         for(int i=0;i<s1.size();i++){
-            if(s1[i]=='x' and s2[i]=='y') xy++;
-            else if(s1[i]=='y' and s2[i]=='x') yx++;
+            if(s1[i]=='x' and s2[i]=='y') 
+                xy++;
+            else if(s1[i]=='y' and s2[i]=='x') 
+                yx++;
         }
-        if(xy%2 != yx%2) return -1;
+        if(xy%2 != yx%2) 
+            return -1;
         int ans = xy/2+yx/2+(xy%2)*2;
         return ans;
     }
 };
-
 
 Example 1:
 
