4eb6d90946cf05a12ecb2a1fb56a4b2f63e18720
Ankit Kumar <anshkrrr@gmail.com>
2022-06-18 21:48:48 +0530

    Update 0048(0078). Subsets.cc

--- LeetCodeSolution/Backtracking/0048(0078). Subsets.cc
+++ LeetCodeSolution/Backtracking/0048(0078). Subsets.cc
@@ -16,6 +16,28 @@ public:
         return ans;
     }
 };
+// try this one
+//similar to subsets 2
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void helper(int idx,vector<int>& nums,vector<int>& subset){
+        ans.push_back(subset); // try generating of every size = 0,1,2,3...
+           
+        for(int i=idx;i<nums.size();i++){
+            if(i==idx || nums[i]!=nums[i-1]){ //first element h to daal do uske baad don't add duplicates
+                subset.push_back(nums[i]);
+                helper(i+1,nums,subset);  // size+1
+                subset.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> subset;
+        helper(0,nums,subset);
+        return ans;
+    }
+};
 
 
 
