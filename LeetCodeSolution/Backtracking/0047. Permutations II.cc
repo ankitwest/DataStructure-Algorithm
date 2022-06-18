16b1525b85cdd394bf0a30589b1d3efdc23d03fa
Ankit Kumar <anshkrrr@gmail.com>
2022-06-18 21:55:39 +0530

    Update 0047. Permutations II.cc

--- LeetCodeSolution/Backtracking/0047. Permutations II.cc
+++ LeetCodeSolution/Backtracking/0047. Permutations II.cc
@@ -26,3 +26,34 @@ public:
 
 // TC - n*n!
 // we can also use set , but TC will be (log n) extra
+
+
+
+//latest
+class Solution {
+public:
+    //*** in this pass by refernce gives wrong answer   and also swap
+    vector<vector<int>> ans;
+    void helper(int idx,vector<int> nums,int n){
+        if(idx==n){
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int i=idx;i<n;i++){
+            // if(i>idx and nums[i]==nums[idx]) continue;
+            if(i==idx || nums[i]!=nums[idx]){ 
+                swap(nums[i],nums[idx]);
+                helper(idx+1,nums,n);
+                // swap(nums[i],nums[idx]);
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        helper(0,nums,n);
+        return ans;
+    }
+};
+
