771b39bd793d6dcfc634c422c9d5897399da9d10
Ankit Kumar <anshkrrr@gmail.com>
2022-06-21 03:11:49 +0530

    Update largest-bst.cc

--- GFG/BinarySearchTree/largest-bst.cc
+++ GFG/BinarySearchTree/largest-bst.cc
@@ -6,27 +6,65 @@ struct Bst{
 };
 class Solution{
     public:
-    /*You are required to complete this method */
-    // Return the size of the largest sub-tree which is also a BST
-    Bst largestBstFun(Node* root){
+    Bst largestBstFun(Node *root){
+        if(!root)
+            return {true,0,INT_MAX,INT_MIN};
+        
+        Bst l = largestBstFun(root->left);
+        Bst r = largestBstFun(root->right);
+        
+        if(l.isBst and r.isBst and root->data > l.max and root->data < r.min){
+            return {true,l.size + r.size + 1, min(l.min,root->data), max(root->data,r.max)};
+        }
+        
+        return {false,max(l.size,r.size),-1,-1};  // min,max;
+    }
+    int largestBst(Node *root)
+    {
+        return largestBstFun(root).size;
+    }
+};
+
+
+
+// TC - O(N) postorder 
+// SC - O(1) 
+
+
+ //postorder call because will have to check left -> right and then at last root
+class Bst{
+public:    
+    bool isBst;
+    int size;
+    int min;
+    int max;
+    Bst(bool isBst,int size,int min,int max){
+        this->isBst = isBst;
+        this->size = size;
+        this->min = min;    
+        this->max = max;
+        
+    }
+};
+class Solution{
+    public:
+   
+    Bst largestBstFun(Node *root){
         if(!root) {
-            Bst b = {true,0,INT_MAX,INT_MIN};
-            return b;
+            return Bst(true,0,INT_MAX,INT_MIN);
         }
         Bst l=largestBstFun(root->left);
         Bst r=largestBstFun(root->right);
         
-        if(l.isBst and r.isBst and l.max<root->data and r.min>root->data){
-            Bst b = {true, 1+l.size+r.size , min(l.min,root->data) , max(r.max,root->data)};
-            return b;
+        if(l.isBst and r.isBst and l.max<root->data and root->data<r.min){
+            return Bst(true, 1+l.size+r.size , min(l.min,root->data) , max(r.max,root->data));
+     
         }
-        Bst b = {false,max(l.size,r.size),-1,-1};
-        return b;
+        return Bst(false,max(l.size,r.size),-1,-1);  //min,max
     }
     int largestBst(Node *root)
     {
-        Bst b =largestBstFun(root);
-        return b.size;
+        return largestBstFun(root).size;
     }
 };
 Expected Time Complexity: O(N).
