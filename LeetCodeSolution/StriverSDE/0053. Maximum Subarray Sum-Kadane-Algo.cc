a6237f87ac340ba9400864cb65aabd3c4327c71e
Ankit Kumar <anshkrrr@gmail.com>
2022-06-21 11:56:38 +0530

    Create 0053. Maximum Subarray Sum-Kadane-Algo.cc

---
+++ LeetCodeSolution/StriverSDE/0053. Maximum Subarray Sum-Kadane-Algo.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0; int maxi = INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+            maxi = max(sum,maxi);
+            if(sum<0) sum = 0;
+        }
+        return maxi;
+    }
+};
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
