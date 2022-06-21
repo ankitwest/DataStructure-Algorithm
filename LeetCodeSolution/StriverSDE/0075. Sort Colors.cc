97530bb2002eeaf51bfb2f3be83832fef2b54ad1
Ankit Kumar <anshkrrr@gmail.com>
2022-06-21 11:50:03 +0530

    Create 0075. Sort Colors.cc

---
+++ LeetCodeSolution/StriverSDE/0075. Sort Colors.cc
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0;int high = n-1;
+        int mid = 0;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                 mid++,low++;
+            }
+            else if(nums[mid]==1){
+                 mid++;
+            }
+            else if(nums[mid]==2){
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+    }
+};
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2: 
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
