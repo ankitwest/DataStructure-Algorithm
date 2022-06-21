470377435521a649f612c654c99dd92733a90067
Ankit Kumar <anshkrrr@gmail.com>
2022-06-22 02:10:03 +0530

    Update 0235. Lowest Common Ancestor of a Binary Search Tree.cc

--- LeetCodeSolution/BST/0235. Lowest Common Ancestor of a Binary Search Tree.cc
+++ LeetCodeSolution/BST/0235. Lowest Common Ancestor of a Binary Search Tree.cc
@@ -1,4 +1,5 @@
-class Solution {
+
+class Solution1 {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(!root) return NULL;
@@ -16,5 +17,39 @@ public:
     }
 };
 
+// recursive simple
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return NULL;
+        
+        if((root->val > p->val) and (root->val > q->val))
+            return lowestCommonAncestor(root->left, p, q);
+        if((root->val < p->val) and (root->val < q->val))
+            return lowestCommonAncestor(root->right, p, q);
+        
+        return root;
+    }
+};
+
+//iterative
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* curr = root;
+        while(true){
+            if(curr->val > p->val and curr->val > q->val)
+                curr = curr->left;
+            else if(curr->val < p->val and curr->val < q->val)
+                curr = curr->right;
+            else
+                break;
+        }      
+        return curr;
+    }
+};
+
 
 // example - 2,6,8  p = 2 q = 8
+
+
