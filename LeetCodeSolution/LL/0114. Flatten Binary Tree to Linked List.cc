5187b5c2120100d163727e73c9134822f1086523
Ankit Kumar <anshkrrr@gmail.com>
2022-06-22 15:23:35 +0530

    Update 0114. Flatten Binary Tree to Linked List.cc

--- LeetCodeSolution/LL/0114. Flatten Binary Tree to Linked List.cc
+++ LeetCodeSolution/LL/0114. Flatten Binary Tree to Linked List.cc
@@ -1,50 +1,101 @@
 
-
-//Morris Traversal
-class Solution {
-public:     //  O(1) space
-    void flatten(TreeNode* root) {
-        TreeNode* curr = root;
-        
-        while(curr){
-            if(curr->left){
-                TreeNode* prev = curr->left;
-                while(prev->right)
-                    prev = prev->right;
-                
-                prev->right = curr->right;
-                curr->right = curr->left;
-                curr->left = NULL;
-            }
-            curr = curr->right;
-        }
-    }
-};
-
+// Note:
 // if u want to reach at the last node use while(curr->next)
 
-
-class Solution2 {
-public:       // recursion
+//Morris Traversal
+// TC - Iterative O(N)
+// SC - O(1) space
+// preorder
+class Solution {
+public:     
     void flatten(TreeNode* root) {
-        if(!root or (!root->left and !root->right))  return;
+        // TreeNode* curr = root;
         
-        if(root->left){
-            flatten(root->left);
-            TreeNode* tempRight = root->right;
-            
-            root->right = root->left;
-            root->left = NULL;
-            
-            auto t = root->right;
-            while(t->right) 
-                t = t->right;
-            
-            t->right = tempRight;
+        while(root)
+        {
+            if(root->left)
+            {
+                TreeNode* last = root->left;   //last guy of left subtree
+                while(last->right)
+                    last = last->right;
+                
+                last->right = root->right;
+                root->right = root->left;
+                root->left = NULL;
+            }
+            root = root->right;
         }
-        flatten(root->right);
     }
 };
+
+// TC - O(N)  SC - O(N)
+class Solution2 {
+public:       
+    void flatten(TreeNode* root) {
+        if (!root) return;
+        stack<TreeNode*> st;
+        st.push(root);
+        while (!st.empty()){
+            TreeNode *curr = st.top(); st.pop();
+            
+            if(curr->right)  
+                st.push(curr->right);
+            if(curr->left)   
+                st.push(curr->left);
+            if(!st.empty()) 
+                 curr->right = st.top();
+            curr->left = NULL;       // dont forget this!! 
+        }
+    }
+};
+
+
+// class Solution2 {
+// public:       // recursion
+//     void flatten(TreeNode* root) {
+//         if(!root or (!root->left and !root->right))  return;
+        
+//         if(root->left){
+//             flatten(root->left);
+//             TreeNode* tempRight = root->right;
+            
+//             root->right = root->left;
+//             root->left = NULL;
+            
+//             auto t = root->right;
+//             while(t->right) 
+//                 t = t->right;
+            
+//             t->right = tempRight;
+//         }
+//         flatten(root->right);
+//     }
+// };
+
+
+
+
+//rahul
+//recusion
+// reverse postorder
+// TC - O(N)  SC - O(N)
+class Solution3 {
+    TreeNode *prev = NULL;
+public:   
+     void flatten(TreeNode *root) {
+        if(!root) return;
+        
+        flatten(root->right);
+        flatten(root->left);
+        
+        root->right = prev;
+        root->left = NULL;
+        prev = root;
+    }
+};
+
+
+
 
 [1,3,4,null,null,2,null,null,5]
 []
