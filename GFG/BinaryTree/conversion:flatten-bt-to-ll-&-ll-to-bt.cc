ba5a4158b8f9b1a98751b6a9d019babd09c88f76
Ankit Kumar <anshkrrr@gmail.com>
2022-06-22 15:30:25 +0530

    Update conversion:flatten-bt-to-ll-&-ll-to-bt.cc

--- GFG/BinaryTree/conversion:flatten-bt-to-ll-&-ll-to-bt.cc
+++ GFG/BinaryTree/conversion:flatten-bt-to-ll-&-ll-to-bt.cc
@@ -1,47 +1,97 @@
 // Flatten binary tree to linked list
-class Solution
+// best 
+//Morris Traversal
+// TC - Iterative O(N)
+// SC - O(1) space
+// preorder
+class Solution11
 {
     public:
     void flatten(Node *root)
     {
-        if(!root or (!root->left and !root->right))
-            return ;
-        if(root->left){
-            flatten(root->left);
-            Node* tempRight = root->right;
-            root->right = root->left;
-            root->left = NULL;
-            
-            Node* t = root->right;
-            while(t->right){
-                t = t->right;
+         while(root)
+        {
+            if(root->left)
+            {
+                Node* last = root->left;   //last guy of left subtree
+                while(last->right)
+                    last = last->right;
+                
+                last->right = root->right;
+                root->right = root->left;
+                root->left = NULL;
             }
-            t->right = tempRight;
+            root = root->right;
         }
-        flatten(root->right);
+    }
+    
+};
+
+// TC - O(N)  SC - O(N)
+class Solution
+{
+public:
+    void flatten(Node *root)
+    {
+         if (!root) return;
+        stack<Node*> st;
+        st.push(root);
+        while (!st.empty()){
+            Node *curr = st.top(); st.pop();
+            
+            if(curr->right)  
+                st.push(curr->right);
+            if(curr->left)   
+                st.push(curr->left);
+            if(!st.empty()) 
+                 curr->right = st.top();
+            curr->left = NULL;       // dont forget this!! 
+        }
     }
 };
 
-class Solution
-{
-    public:
-    void flatten(Node *root)
-    {
-        Node* curr = root;
-        Node* prev = NULL;
-        while(curr){
-            if(curr->left){
-                prev = curr->left;
-                while(prev->right)
-                    prev = prev->right;
-                prev->right = curr->right;
-                curr->right = curr->left;
-                curr->left = NULL;
-            }
-            curr = curr->right;
-        }
+//recusion
+// reverse postorder
+// TC - O(N)  SC - O(N)
+class Solution2
+{  
+public:
+     Node *prev = NULL;
+     void flatten(Node *root) {
+        if(!root) return;
+        
+        flatten(root->right);
+        flatten(root->left);
+        
+        root->right = prev;
+        root->left = NULL;
+        prev = root;
     }
 };
+// not able to learn
+// class Solution
+// {
+//     public:
+//     void flatten(Node *root)
+//     {
+//         if(!root or (!root->left and !root->right))
+//             return ;
+//         if(root->left){
+//             flatten(root->left);
+//             Node* tempRight = root->right;
+//             root->right = root->left;
+//             root->left = NULL;
+            
+//             Node* t = root->right;
+//             while(t->right){
+//                 t = t->right;
+//             }
+//             t->right = tempRight;
+//         }
+//         flatten(root->right);
+//     }
+// };
+
 Input : 
           1
         /   \
