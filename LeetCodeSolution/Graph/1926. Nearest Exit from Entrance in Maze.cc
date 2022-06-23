b5dd3b43ba06ec950df364ffb16d17b42745c2b0
Ankit Kumar <anshkrrr@gmail.com>
2022-06-24 04:03:40 +0530

    Update 1926. Nearest Exit from Entrance in Maze.cc

--- LeetCodeSolution/Graph/1926. Nearest Exit from Entrance in Maze.cc
+++ LeetCodeSolution/Graph/1926. Nearest Exit from Entrance in Maze.cc
@@ -32,3 +32,67 @@ public:
         return -1;
     }
 };
+
+
+//sau
+class Solution {
+public:
+    int dx[4] = {-1 , 1 , 0 , 0};
+    int dy[4] = {0 , 0 , -1 , 1};
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        int n = maze.size() , m = maze[0].size();
+        int x = entrance[0] , y = entrance[1];
+        
+        vector<vector<int>> v(n , vector<int>(m , INT_MAX));
+        
+        v[x][y] = 0;
+        set<pair<int,pair<int,int>>> s;
+        
+        s.insert({0 , {x , y}});
+        
+        while(s.size())
+        {
+            auto tp = *s.begin();
+            s.erase(s.begin());
+            auto dis = tp.first;
+            int x1 = tp.second.first;
+            int y1 = tp.second.second;
+            
+            for(int i=0;i<4;i++)
+            {
+                int a = x1 + dx[i];
+                int b = y1 + dy[i];
+                
+                if(a < n && b < m && a >= 0 && b >= 0 && maze[a][b] != '+')
+                {
+                    if(v[a][b] > v[x1][y1] + 1)
+                    {
+                        v[a][b] = (v[x1][y1] + 1);
+                        s.insert({v[a][b] , {a , b}});
+                    }
+                }
+            }
+        }
+        
+        int ans = INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if((i == n - 1 || j == m - 1 || i == 0 || j == 0))
+                {
+                    if(i != x)
+                        ans = min(ans , v[i][j]);
+                    if(j != y)
+                        ans = min(ans , v[i][j]);
+                }
+                     
+            }
+            // cout<<"\n";
+        }
+        // cout<<"\n";
+        
+        return ans == INT_MAX ? -1 : ans;
+        
+    }
+};
