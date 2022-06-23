cd4052128b6b950c9898a1e3e58f20d6fbef4a1d
Ankit Kumar <anshkrrr@gmail.com>
2022-06-24 02:35:52 +0530

    Update 1249. Minimum Remove to Make Valid Parentheses.cc

--- LeetCodeSolution/Stack/1249. Minimum Remove to Make Valid Parentheses.cc
+++ LeetCodeSolution/Stack/1249. Minimum Remove to Make Valid Parentheses.cc
@@ -1,19 +1,19 @@
-class Solution {
+class Solution1 {
 public:
     string minRemoveToMakeValid(string s) {
         stack<int> st;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='(')
+            if(s[i]=='('){
                 st.push(i);
-            else if(s[i]==')'){
-                if(st.empty())
-                    s[i]='#';
+            }else if(s[i]==')'){
+                if(st.empty()) s[i]='#';
                 else st.pop();
             }
         }
         
         while(!st.empty()){
-            s[st.top()] = '#'; st.pop();
+            int top = st.top(); st.pop();
+            s[top] = '#'; 
         }
         
         string ans;
@@ -24,34 +24,26 @@ public:
     }
 };
 
-Input: s = "lee(t(c)o)de)"
-Output: "lee(t(c)o)de"
-Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
-  
-  
-//   without stack
 
-M -2
+// M -2
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         int ctr=0;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='(')
+            if(s[i]=='('){
                 ctr++;
-            else if(s[i]==')'){
-                if(ctr==0)
-                    s[i]='#';
+            }else if(s[i]==')'){
+                if(ctr==0) s[i]='#';
                 else ctr--;
             }
         }
         ctr=0;
         for(int i=s.size()-1;i>=0;i--){
-            if(s[i]==')')
+            if(s[i]==')'){
                 ctr++;
-            else if(s[i]=='('){
-                if(ctr==0)
-                    s[i]='#';
+            }else if(s[i]=='('){
+                if(ctr==0) s[i]='#';
                 else ctr--;
             }
         }
