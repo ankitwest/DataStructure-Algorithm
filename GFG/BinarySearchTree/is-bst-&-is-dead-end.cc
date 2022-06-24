0b7cfa13e44fbe3a10c9eceaeada583993e6c025
Ankit Kumar <anshkrrr@gmail.com>
2022-06-25 04:50:08 +0530

    Update is-bst-&-is-dead-end.cc

--- GFG/BinarySearchTree/is-bst-&-is-dead-end.cc
+++ GFG/BinarySearchTree/is-bst-&-is-dead-end.cc
@@ -4,23 +4,46 @@
 class Solution{
   public:
     //Function to check whether a Binary Tree is BST or not.
-    void checkBST(Node* root,int l,int r,bool &flag){
+     bool flag=-1;
+    void checkBST(Node* root,int l,int r){
         if(!root) return ;
         if(l>root->data or r<root->data)
             {flag =0; return ;}
            
-        checkBST(root->left,l,root->data-1,flag);
-        checkBST(root->right,root->data+1,r,flag);
+        checkBST(root->left,l,root->data-1);
+        checkBST(root->right,root->data+1,r);
     }
     bool isBST(Node* root) 
     {
-      bool flag=-1;
-       checkBST(root,1,INT_MAX,flag);
+       checkBST(root,1,INT_MAX);
        return flag;
     }
 };
 
 
+
+
+class Solution{
+  public:
+  bool solve(Node* root,int min,int max)
+    {
+        if(!root) 
+            return true;
+        
+        bool left = solve(root->left,min,root->data-1);
+        bool right = solve(root->right,root->data+1,max);
+        
+        if(root->data < min or root->data > max or left==false or right==false)
+            return false;
+           
+         return  true;
+    }
+public:
+    bool isBST(Node* root) 
+    {
+       return solve(root,INT_MIN,INT_MAX);
+    }
+};
 
 // Check for Dead End
 // Using Range
