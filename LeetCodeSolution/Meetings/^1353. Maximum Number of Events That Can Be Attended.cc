99d3ffe486c6b22ec32ab318e463c987310c5c00
Ankit Kumar <anshkrrr@gmail.com>
2022-07-05 00:29:43 +0530

    Update and rename 1353. Maximum Number of Events That Can Be Attended.cc to ^1353. Maximum Number of Events That Can Be Attended.cc

--- LeetCodeSolution/Meetings/^1353. Maximum Number of Events That Can Be Attended.cc
+++ LeetCodeSolution/Meetings/^1353. Maximum Number of Events That Can Be Attended.cc
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
-//         sort(events.begin(),events.end());
-//         priority_queue<int,vector<int>,greater<int>> pq;
+        sort(events.begin(),events.end());
+        priority_queue<int,vector<int>,greater<int>> pq;
         
-//         int ans=0; int i=0;
-//         for(int d=1;d<=100000;d++){
-//             while(!pq.empty() and pq.top()<d)
-//                 pq.pop();
-//             while(i<events.size() and events[i][0]==d){
-//                 pq.push(events[i][1]); i++;
-//             }
+        int ans=0; int i=0;
+        for(int d=1;d<=100000;d++){
+            while(!pq.empty() and pq.top()<d)
+                pq.pop();
+            while(i<events.size() and events[i][0]==d){
+                pq.push(events[i][1]); i++;
+            }
             
-//             if(!pq.empty()) pq.pop(),ans++;
-//         }
-//         return ans;
-        
-        // M-2
+            if(!pq.empty()) pq.pop(),ans++;
+        }
+        return ans;
+    }
+};
+    
+// M-2
+class Solution2 {
+public:
+    int maxEvents(vector<vector<int>>& events) {
         sort(events.begin(),events.end());
         priority_queue<int,vector<int>,greater<int>> pq;
         
