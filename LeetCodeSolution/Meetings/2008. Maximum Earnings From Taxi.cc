c7f0a4cad24c4dd76505d4391d0c90fe74468b47
Ankit Kumar <anshkrrr@gmail.com>
2022-07-04 23:52:49 +0530

    Update 2008. Maximum Earnings From Taxi.cc

--- LeetCodeSolution/Meetings/2008. Maximum Earnings From Taxi.cc
+++ LeetCodeSolution/Meetings/2008. Maximum Earnings From Taxi.cc
@@ -1,12 +1,11 @@
-class Solution {
-public:
+class Solution1 {
     int nextPass(vector<vector<int>>& rides,int i){
         int n = rides.size();
         int low = i+1; int high = n-1;
-        int ans = n;
+        int ans = n; int end = rides[i][1];
         while(low<=high){
-            int mid=low+(high-low)/2;
-            if(rides[mid][0]>=rides[i][1])
+            int mid = low+(high-low)/2;
+            if(rides[mid][0] >= end)
                 ans = mid, high = mid-1;
             else 
                 low = mid+1;
@@ -21,18 +20,56 @@ public:
         if(dp[i]!=-1) 
             return dp[i];
         
-        int next = nextPass(rides,i);
+        int next_index = nextPass(rides,i);
         
         // pick , not pick
-        long long incl = rides[i][1]-rides[i][0]+rides[i][2] + solve(rides,next);
+        int dollar = rides[i][1]-rides[i][0]+rides[i][2];
+        long long incl = dollar + solve(rides,next_index);
         long long excl = solve(rides,i+1);
         
-        return dp[i]=max(incl,excl);
+        return dp[i] = max(incl,excl);
     }
-    long long maxTaxiEarnings(int n, vector<vector<int>>& rides) {
-    sort(rides.begin(),rides.end());
-    dp.resize(rides.size(),-1);
-    return solve(rides,0);
+public:
+    long long maxTaxiEarnings(int t, vector<vector<int>>& rides) {
+        sort(rides.begin(),rides.end());
+        int n = rides.size();
+        dp.resize(n,-1);
+        return solve(rides,0);
+    }
+};
+
+
+
+//tabulation
+class Solution {
+    int nextPass(vector<vector<int>>& rides,int i){
+        int n = rides.size();
+        int low = i+1; int high = n-1;
+        int ans = n; int end = rides[i][1];
+        while(low <= high){
+            int mid = low+(high-low)/2;
+            if(rides[mid][0] >= end)
+                ans = mid, high = mid-1;
+            else 
+                low = mid+1;
+        }
+        return ans;
+    }
+public:
+    long long maxTaxiEarnings(int t, vector<vector<int>>& rides) {
+        sort(rides.begin(),rides.end());
+        
+        int n = rides.size();
+        long long dp[n+1]; 
+        memset(dp,0,sizeof(dp));
+        dp[n] = 0;
+        
+        for(int i=n-1;i>=0;i--){
+            int dollar = rides[i][1]-rides[i][0]+rides[i][2];
+            int next_index = nextPass(rides,i);
+            dp[i] = max(dp[i+1], dp[next_index] + dollar);
+        }
+        return dp[0];
     }
 };
 
