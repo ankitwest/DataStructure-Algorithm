ae1322979409aaa2e28ae3217bb376a52d055de1
Ankit Kumar <anshkrrr@gmail.com>
2022-07-04 17:46:30 +0530

    Update sudoku-solver.cc

--- GFG/Backtracking/sudoku-solver.cc
+++ GFG/Backtracking/sudoku-solver.cc
@@ -24,47 +24,47 @@ class Solution{
         return true;
         
     }
-//         bool sudoku(int i,int j,int grid[N][N]){
-//          for(int i=0;i<N;i++){
-//             for(int j=0;j<N;j++){
-//                 if(grid[i][j]==0){
-//                     for(int choice=1;choice<=9;choice++){
-//                         if(valid(i,j,grid,choice)){
-//                             grid[i][j]=choice;
-//                             if(sudoku(i,j,grid)) return true;
-//                             grid[i][j]=0;
-//                         }
-//                     }//for loop
-//                     return false;
-//                 }
-//             }
-//         }
-//         return true; 
-//     }
-    
-    bool sudoku(int i,int j,int grid[N][N]){
-        if(i==N) return true;
-            
-        int new_i=0,new_j=0;
-        if(j==N-1) new_i=i+1,new_j=0;
-        else new_i=i,new_j=j+1;
-        
-        //Already filled
-        if(grid[i][j]!=0){
-            return sudoku(new_i,new_j,grid);
-        }
-        else{  // Empty boxes
-            for(int k=1;k<=9;k++){
-                if(valid(i,j,grid,k)){
-                  grid[i][j]=k;
-                  if(sudoku(new_i,new_j,grid)) return true;
-                  grid[i][j]=0;
+        bool sudoku(int i,int j,int grid[N][N]){
+         for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(grid[i][j]==0){
+                    for(int choice=1;choice<=9;choice++){
+                        if(valid(i,j,grid,choice)){
+                            grid[i][j]=choice;
+                            if(sudoku(i,j,grid)) return true;
+                            grid[i][j]=0;
+                        }
+                    }//for loop
+                    return false;
                 }
             }
-            return false;
         }
-        return true;
+        return true; 
     }
+    
+//     bool sudoku(int i,int j,int grid[N][N]){
+//         if(i==N) return true;
+            
+//         int new_i=0,new_j=0;
+//         if(j==N-1) new_i=i+1,new_j=0;
+//         else new_i=i,new_j=j+1;
+        
+//         //Already filled
+//         if(grid[i][j]!=0){
+//             return sudoku(new_i,new_j,grid);
+//         }
+//         else{  // Empty boxes
+//             for(int k=1;k<=9;k++){
+//                 if(valid(i,j,grid,k)){
+//                   grid[i][j]=k;
+//                   if(sudoku(new_i,new_j,grid)) return true;
+//                   grid[i][j]=0;
+//                 }
+//             }
+//             return false;
+//         }
+//         return true;
+//     }
     bool SolveSudoku(int grid[N][N])  
     { 
         return sudoku(0,0,grid);
@@ -80,53 +80,6 @@ class Solution{
     }
 };
 Expected Time Complexity: O(9^(N*N)).
-
-
-// LeetCode  copied
-
-class Solution {
-public:
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
-    }
-    
-    bool solve(vector<vector<char>>& board){
-        for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[0].size(); j++){
-                if(board[i][j] == '.'){
-                    for(char c = '1'; c <= '9'; c++){
-                        if(isValid(board, i, j, c)){
-                            board[i][j] = c; 
-                            
-                            if(solve(board))
-                                return true; 
-                            else
-                                board[i][j] = '.'; 
-                        }
-                    }
-                    
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    
-    bool isValid(vector<vector<char>>& board, int row, int col, char c){
-        for(int i = 0; i < 9; i++) {
-            if(board[i][col] == c) 
-                return false; 
-            
-            if(board[row][i] == c) 
-                return false; 
-            
-            if(board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) 
-                return false; 
-        }
-        return true;
-    }
-};
-
 
 
 
