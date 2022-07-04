20152cf9d61d88ca90a2dd38923da76909fb98cd
Ankit Kumar <anshkrrr@gmail.com>
2022-07-05 01:05:53 +0530

    Update ^Rabin-Karp Algorithm[Hashing].cc

--- GFG/String/^Rabin-Karp Algorithm[Hashing].cc
+++ GFG/String/^Rabin-Karp Algorithm[Hashing].cc
@@ -34,6 +34,17 @@ public:
         }
         };
 
+How does the above expression work? 
+
+This is simple mathematics, we compute decimal value of current window from previous window. 
+For example pattern length is 3 and string is “23456” 
+You compute the value of first window (which is “234”) as 234. 
+How  will you compute value of next window “345”? You will do (234 – 2*100)*10 + 5 and get 345.
+
+
+        
+        
+
 Input:
 S = "batmanandrobinarebat", pat = "bat"
 Output: 1 18
