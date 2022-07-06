f623d30632e12266ced8b1da7d98957a20e7e128
Ankit Kumar <anshkrrr@gmail.com>
2022-07-06 16:27:58 +0530

    Update 2327. Number of People Aware of a Secret.cc

--- LeetCodeSolution/Contest/weekly-300/2327. Number of People Aware of a Secret.cc
+++ LeetCodeSolution/Contest/weekly-300/2327. Number of People Aware of a Secret.cc
@@ -29,3 +29,13 @@ public:
 1
 3
 */
+
+Input: n = 6, delay = 2, forget = 4
+Output: 5
+Explanation:
+Day 1: Suppose the first person is named A. (1 person)
+Day 2: A is the only person who knows the secret. (1 person)
+Day 3: A shares the secret with a new person, B. (2 people)
+Day 4: A shares the secret with a new person, C. (3 people)
+Day 5: A forgets the secret, and B shares the secret with a new person, D. (3 people)
+Day 6: B shares the secret with E, and C shares the secret with F. (5 people)
