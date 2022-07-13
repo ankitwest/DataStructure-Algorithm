48574188e526118e8ef9df153b3bb2fb3451334b
Ankit Kumar <anshkrrr@gmail.com>
2022-07-13 17:38:16 +0530

    Update and rename Subarrays with equal 1s and 0s .cc to Subarrays with equal 1s and 0s-&-largest subarray.cc

--- GFG/Hashing/Subarrays with equal 1s and 0s-&-largest subarray.cc
+++ GFG/Hashing/Subarrays with equal 1s and 0s-&-largest subarray.cc
@@ -28,3 +28,31 @@ Output: 8
 Explanation: The index range for the 8 
 sub-arrays are: (0, 1), (2, 3), (0, 3), (3, 4), 
 (4, 5) ,(2, 5), (0, 5), (1, 6)
+
+  
+  
+  
+  
+  Q   Largest subarray of 0's and 1's
+  
+  class Solution {
+  public:
+    int maxLen(int arr[], int n)
+    {
+        int max_len = 0; int sum = 0;
+        unordered_map<int,int> m;
+        m[0] = -1;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0)
+                sum -= 1;
+            else if(arr[i]==1)
+                sum += 1;
+                
+            if(m.count(sum))
+                max_len = max(max_len,i - m[sum]);
+            else 
+                m[sum] = i;
+        }
+        return max_len;
+    }
+};
