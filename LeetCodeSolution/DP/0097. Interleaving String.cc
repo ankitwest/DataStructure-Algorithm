f85a6efa6e8da9903ed0efaa0f09831f9d2d6bf0
Ankit Kumar <anshkrrr@gmail.com>
2022-07-15 12:08:22 +0530

    Update 0097. Interleaving String.cc

--- LeetCodeSolution/DP/0097. Interleaving String.cc
+++ LeetCodeSolution/DP/0097. Interleaving String.cc
@@ -1,44 +1,3 @@
-//Approach-2 (Recursion + Memoization (i, j))
-class Solution2 {
-public:
-    int m, n, N;
-    int t[101][101];
-    
-    bool check(string s1, string s2, int i, int j, string s3) {
-        if(i == n && j == m && (i+j) == N) 
-            return true;
-            
-        if(t[i][j] != -1)
-            return t[i][j];
-        
-        bool a = false, b = false;
-        int k = i + j;
-        if(s1[i]==s3[k]){
-            a = check(s1, s2, i+1, j, s3);
-        }
-        if(s2[j]==s3[k]){
-            b = check(s1, s2, i, j+1, s3);
-        }
-        
-        return t[i][j] = a | b;
-    }
-    bool isInterleave(string s1, string s2, string s3) {
-        m = s1.length();
-        n = s2.length();
-        N = s3.length();
-        memset(t, -1, sizeof(t));
-        
-        if(m + n != N)
-            return false;
-        return check(s1,s2,0,0,s3);
-    }
-    
-};
-
-        // return t[i][j] = ((s1[i]==s3[i+j] && check(s1, s2, i+1, j, s3)) | (s2[j]==s3[i+j] && check(s1, s2, i, j+1, s3)));
-
-
-
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
@@ -53,27 +12,82 @@ public:
         
         for(int i=0;i<=n;i++){
              for(int j=0;j<=m;j++){
-                 if(i==0 and j==0)  t[i][j] = 1;
-                 else if(i==0){
-                     if(s2[j-1]==s3[i+j-1])
-                         t[i][j] = t[i][j-1];
+                 if(i==0 and j==0){ 
+                     t[i][j] = 1;
+                 }else if(i==0){
+                     if(s2[j-1]==s3[i+j-1])  t[i][j] = t[i][j-1];
                  }
                  else if(j==0){
-                     if(s1[i-1]==s3[i+j-1])
-                         t[i][j] = t[i-1][j];
+                     if(s1[i-1]==s3[i+j-1]) t[i][j] = t[i-1][j];
                  }
-                 else if(s1[i-1]==s3[i+j-1] and s2[j-1]!=s3[i+j-1])
-                     t[i][j] = t[i-1][j];
-                 else if(s1[i-1]!=s3[i+j-1] and s2[j-1]==s3[i+j-1])
-                     t[i][j] = t[i][j-1];
-                 else if(s1[i-1]==s3[i+j-1] and s2[j-1]==s3[i+j-1])
-                     t[i][j] = t[i-1][j] | t[i][j-1];
+                 else {
+                     if(s1[i-1]==s3[i+j-1]) t[i][j] |= t[i-1][j];
+                     if(s2[j-1]==s3[i+j-1]) t[i][j] |= t[i][j-1];
+                 }
              }
         }
-        
         return t[n][m];
     }
 };
+
+
+//Approach-2 (Recursion + Memoization (i, j))
+class Solution2 {
+public:
+    int m, n, N;
+    int t[101][101];
+    
+    bool check(string s1, string s2, int i, int j, string s3,int k) {
+        if(i == n and j == m and k == N) 
+            return true;
+            
+        if(t[i][j] != -1)
+            return t[i][j];
+        
+        bool a = false, b = false;
+        if(s1[i]==s3[k])
+            a = check(s1, s2, i+1, j, s3, k+1);
+        
+        if(s2[j]==s3[k])
+            b = check(s1, s2, i, j+1, s3, k+1);
+        
+        return t[i][j] = a | b;
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        m = s1.length();
+        n = s2.length();
+        N = s3.length();
+        memset(t, -1, sizeof(t));
+        
+        if(m + n != N)
+            return false;
+        return check(s1,s2,0,0,s3,0);
+    }
+};
+
+
+class Solution3 {
+    int m, n, N;
+    bool solve(string s1,string s2,int i,int j,string s3,int k){
+        if(i == n and j == m and k == N) 
+            return true;
+        
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+            
+        return dp[i][j] = ((s1[i]==s3[i+j] && solve(s1, s2, i+1, j, s3, k+1)) || (s2[j]==s3[i+j] && solve(s1, s2, i, j+1, s3, k+1)));
+    }
+public:
+    vector<vector<int>> dp;
+    bool isInterleave(string s1, string s2, string s3) {
+        n = s1.size(); m = s2.size(); N = s3.size();
+        dp.resize(n+1,vector<int>(m+1,-1));
+        if(m + n != N)
+            return false;
+        return solve(s1,s2,0,0,s3,0);
+    }
+};
+                 
 
 Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
 Output: true
