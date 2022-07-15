8b27d7cb086a800e550606e8f585132c2ad7328a
Ankit Kumar <anshkrrr@gmail.com>
2022-07-15 12:10:14 +0530

    Update and rename 0-top-50-dp.txt to !Top-50-dp.cc

---
+++ GFG/DP/!Top-50-dp.cc
@@ -0,0 +1,55 @@
+https://www.geeksforgeeks.org/top-50-dynamic-programming-coding-problems-for-interviews/
+
+
+
+
+
+
+Level 2
+Subset Sum Problem
+Longest Common Subsequence
+Longest Increasing Subsequence
+Edit Distance
+Longest Path In Matrix
+Optimal Strategy for a Game
+0-1 Knapsack Problem
+Shortest Common Supersequence
+Partition problem
+Rod Cutting
+Coin change problem
+Word Break Problem
+Dice Throw Problem
+Box Stacking
+Egg Dropping Puzzle
+Max length chain
+Longest Common Substring
+Interleaved strings
+Maximum sum increasing subsequence
+Minimum number of jumps
+Count subsequences of type a^i, b^j, c^k
+Get Minimum Squares
+Nth Fibonacci Number
+Longest Palindromic Substring
+Total Decoding Messages
+Unique BST’s
+Player with max score
+Form a palindrome
+Word Wrap Problem
+Count Palindromic Subsequences
+Minimum time to finish tasks without skipping two consecutive
+
+
+
+
+Level 3
+Minimum Partition
+Boolean Parenthesization Problem
+Matrix Chain Multiplication
+Longest Zig-Zag Sub Sequence
+Maximum Profit
+Minimum Cost Path
+The painter’s partition problem
+Palindrome Partitioning
+Array Partition
+Maximum difference of zeros and ones in binary string
+Count digit groupings of a number
