aeaf2900950a4e9a2e69a6e7d52f86862c289456
Ankit Kumar <anshkrrr@gmail.com>
2022-07-16 15:34:42 +0530

    Update Game of XOR.cc

--- GFG/XOR/Game of XOR.cc
+++ GFG/XOR/Game of XOR.cc
@@ -9,7 +9,19 @@ public:
         }
         return ans;
     }
+    
+    
+    int gameOfXor(int N,int A[]){
+         if(N%2==0)
+           return 0;
+       int ans=0;
+       for(int i=0;i<N;i=i+2){
+           ans=ans^A[i];
+       }
+       return ans;
+    }
 };
+
 
 
 Expected Time Complexity: O(N)
