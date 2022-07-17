ad6e1f0a6a80c7acae9f2bec0e9f3334240eccfe
Ankit Kumar <anshkrrr@gmail.com>
2022-07-17 18:11:58 +0530

    Update the-celebrity-problem.cc

--- GFG/Stack/the-celebrity-problem.cc
+++ GFG/Stack/the-celebrity-problem.cc
@@ -1,32 +1,65 @@
-class Solution{
-  public:
+class Solution
+{
+    public:
     //Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& M, int n) 
     {
-        int no=n;
         stack<int> st;
-        while(n--) st.push(n);
+        for (int i = 0; i < n; i++)
+            st.push(i);
         
-        int i=1;n=no;
-        while(i<n){
+        // Find a potential celebrity
+        while (st.size()>1)
+        {
             int a = st.top();st.pop();
             int b = st.top();st.pop();
             if(M[a][b]==1) st.push(b); 
             else st.push(a);
-            i++;
         }
+         // Potential candidate?
         int ans = st.top();
         
         int ctr=0;
         for(int i=0;i<n;i++){
-            if(i==ans) continue;
-            if(M[i][ans]==1 and M[ans][i]==0) ctr++;
-            else return -1;
+            if(i!=ans and (M[i][ans]==0 or M[ans][i]==1)) 
+            return -1;
         }
-        if(ctr==n-1) ; else return -1; 
         return ans;
     }
 };
+
+
+class Solution2
+{
+public:    
+    int celebrity(vector<vector<int> >& m, int n) {
+        int a=0;
+        int b=n-1;
+        while(a<=b){  
+          if(m[a][b]==1) a++; 
+          else b--;
+        }
+        
+      int k=0,l=0;
+      for(int j=0;j<n;j++)
+      {
+          if(m[a][j]==0)
+          k++;
+      }
+      for(int i=0;i<n;i++)
+      {
+          if(m[i][a]==1)
+          l++;
+      }
+      if(k==n and l==n-1)
+      return a;
+      else
+      return -1;
+    }
+};
+
+
+
 N = 3
 M[][] = {{0 1 0},
          {0 0 0}, 
