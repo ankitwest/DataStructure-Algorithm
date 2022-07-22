d09338643231a9f50616ebca98a7e4656ab9b2bf
Ankit Kumar <anshkrrr@gmail.com>
2022-07-22 21:47:20 +0530

    Update 0044-DP. Wildcard Matching.cc

--- LeetCodeSolution/String/0044-DP. Wildcard Matching.cc
+++ LeetCodeSolution/String/0044-DP. Wildcard Matching.cc
@@ -1,96 +1,113 @@
-class Solution2 {
+class Solution1 {
 public:
     vector<vector<int>> dp;
-    bool solve(string &s,string &p,int i,int j){
+    bool solve(string &pat,string &s,int i,int j){
         if(i<0 and j<0) return true;
         if(i<0 and j>=0) return false;
         
-        if(i>=0 and j<0){
-            for(int i1=0;i1<=i;i1++)
-                if(s[i1]!='*') return false;
+        if(j<0 and i>=0){
+            for(int idx=0;idx<=i;idx++)
+                if(pat[idx]!='*') return false;
             return true;
         }
         
         if(dp[i][j]!=-1)
             return dp[i][j];
         
-        bool ans;
-        if(s[i]==p[j] or s[i]=='?')
-            ans = solve(s,p,i-1,j-1);
-        else if(s[i]=='*')
-            ans = solve(s,p,i-1,j) or solve(s,p,i,j-1);
+        bool ans = false;
+        if(pat[i]==s[j] or pat[i]=='?')
+            ans = solve(pat,s,i-1,j-1);
+        else if(pat[i]=='*')
+            ans = solve(pat,s,i-1,j) or solve(pat,s,i,j-1);
         
         return dp[i][j] = ans;
     }
     bool isMatch(string s, string p) {
-        swap(s,p);
-        int n = s.size(); int m = p.size();
-        dp.resize(n,vector<int>(m,-1));
-        return solve(s,p,n-1,m-1);
+        int s_n = s.size(); int p_n = p.size();
+        dp.resize(p_n,vector<int>(s_n,-1));
+        return solve(p,s,p_n-1,s_n-1);  // 0 based indexing   crt ans
     }
 };
 
 
+/*
+"aab"
+"c*a*b"
+E:false
+
+left c*a should also be exhausted 
+and can only be with *
+*/
 
 
+class Solution2 {
+public:
+    vector<vector<int>> dp;
+    // make changes in this acc to 1 based indexing and then use this code in tabulation
+    bool solve(string &pat,string &s,int i,int j){
+        if(i==0 and j==0) return true;
+        if(i==0 and j>0) return false;
+        if(j==0 and i>0){
+            for(int idx=1;idx<=i;idx++)
+                if(pat[idx-1]!='*') return false;
+            return true;
+        }
+        
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        bool ans = false;
+        if(pat[i-1]==s[j-1] or pat[i-1]=='?')
+            ans = solve(pat,s,i-1,j-1);
+        else if(pat[i-1]=='*')
+            ans = solve(pat,s,i-1,j) or solve(pat,s,i,j-1);
+        
+        return dp[i][j] = ans;
+    }
+    bool isMatch(string s, string p) {
+        // swap(s,p);
+        int s_n = s.size(); int p_n = p.size();
+        dp.resize(p_n+1,vector<int>(s_n+1,-1));
+        return solve(p,s,p_n,s_n);  // 1 based indexing
+    }
+};
 
 
 // Tabulation
 class Solution {
 public:
-    // make changes in this acc to 1 based indexing and then use this code in tabulation
-    
-//     bool solve(string &s,string &p,int i,int j){
-//         if(i==0 and j==0) return true;
-//         if(i==0 and j>0) return false;
-        
-//          if(i>0 and j==0){  
-//             for(int i1=1;i1<=i;i1++)
-//                 if(s[i1-1]!='*') return false;
-//             return true;
-//         }
-        
-//         if(dp[i][j]!=-1)
-//             return dp[i][j];
-        
-//         bool ans;
-//         if(s[i-1]==p[j-1] or s[i-1]=='?')
-//             ans = solve(s,p,i-1,j-1);
-//         else if(s[i-1]=='*')
-//             ans = solve(s,p,i-1,j) or solve(s,p,i,j-1);
-        
-//         return dp[i][j] = ans;
-//     }
-    
-    bool isMatch(string s, string p) {
-        swap(s,p);
-        int n = s.size(); int m = p.size();
-        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
+    bool isMatch(string s, string pat) {
+        int s_n = s.size(); int p_n = pat.size();
+        vector<vector<bool>> dp(p_n+1,vector<bool>(s_n+1,false));
         
         dp[0][0] = true;
-        for(int j=1;j<=m;j++){
+        for(int j = 1; j <= s_n; j++){
             dp[0][j] = false;
         }
         
-        for(int i=1;i<=n;i++){
+        for(int i = 1; i <= p_n; i++){
             bool flag = true;
-            for(int i1=1;i1<=i;i1++)
-                if(s[i1-1]!='*'){ flag = false; break;}
-            
+            for(int idx = 1; idx <= i; idx++){
+                if(pat[idx-1] != '*'){ 
+                    flag = false; 
+                    break;
+                }
+            }
+                
             dp[i][0] = flag;
         }
         
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
+        for(int i = 1; i <= p_n; i++){
+            for(int j = 1; j <= s_n; j++){
                 bool ans = false;
-                 if(s[i-1]==p[j-1] or s[i-1]=='?')
-                    ans = dp[i-1][j-1];
-                else if(s[i-1]=='*')
-                    ans = dp[i-1][j] or dp[i][j-1];
+                 if(pat[i - 1] == s[j - 1] or pat[i - 1] == '?')
+                    ans = dp[i - 1][j - 1];
+                else if(pat[i - 1] == '*')
+                    ans = dp[i - 1][j] or dp[i][j - 1];
         
                 dp[i][j] = ans;
             }
         }
-        return dp[n][m];
+        return dp[p_n][s_n];
     }
 };
