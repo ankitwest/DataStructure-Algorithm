3f1bf82326b0bf27a15e76db6a6edbf693dd1aaf
Ankit Kumar <anshkrrr@gmail.com>
2022-07-22 22:16:18 +0530

    Update Edit Distance .cc

--- GFG/String/Edit Distance .cc
+++ GFG/String/Edit Distance .cc
@@ -1,6 +1,5 @@
 class Solution{
 public:
-  int ss=0,tt=0;
   vector<vector<int>> dp;
     int rec(string s, string t,int i,int j){
         if(i<0 or j<0) return INT_MAX;
@@ -14,16 +13,18 @@ public:
         int ans = 0;
         if(s[i-1]==t[j-1])
             ans = rec(s,t,i-1,j-1);
-        else   // considering all three operations on str1
-            ans = min({rec(s,t,i-1,j),rec(s,t,i,j-1),rec(s,t,i-1,j-1)}) + 1;//remove , insert , replace
+        else    // considering all three operations on str1
+                //remove , insert , replace
+            ans = 1 + min({rec(s,t,i-1,j),rec(s,t,i,j-1),rec(s,t,i-1,j-1)});
         
-        return dp[i][j]=ans;
+        return dp[i][j] = ans;
     }
+  public:
     int editDistance(string s, string t) {
-        ss = s.size(); tt = t.size();
-        dp.resize(ss+1,vector<int>(tt+1,-1));
+        int s_n = s.size(); int t_n = t.size();
+        dp.resize(s_n+1,vector<int>(t_n+1,-1));
         
-        return rec(s,t,ss,tt);
+        return rec(s,t,s_n,t_n);
     }
 };
 
