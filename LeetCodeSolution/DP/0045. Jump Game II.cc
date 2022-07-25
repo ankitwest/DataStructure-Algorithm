807e26d5261e4e8a5adbbe1e1fa95fe12231cf21
Ankit Kumar <anshkrrr@gmail.com>
2022-07-25 23:04:07 +0530

    Update 0045. Jump Game II.cc

--- LeetCodeSolution/DP/0045. Jump Game II.cc
+++ LeetCodeSolution/DP/0045. Jump Game II.cc
@@ -1,24 +1,28 @@
 //better
-
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int maxReachable = 0; int jump=0; int curr=0;
-        int n=nums.size();
+        int n = nums.size();
+        // int maxReachable = 0; //for 0 based indexing
+        // int jump = 0; int curr = 0;
         
-        for(int i=0;i<n;i++){
-            if(maxReachable<i) return -1;
+        int maxReachable = nums[0] + 0;  //for 1 based indexing
+        int jump = 1; int curr = nums[0] + 0;
+        
+        for(int i=1; i<n; i++){
+            if(maxReachable < i) return -1;
             maxReachable = max(maxReachable,nums[i]+i);
             
             if(curr==i){
                 if(i==n-1) continue;
                 jump++;
-                curr=maxReachable;
+                curr = maxReachable;
             }
         }
         return jump;
     }
 };
+
 
 
 
