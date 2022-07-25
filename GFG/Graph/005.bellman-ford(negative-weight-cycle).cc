e911d2dfd6128cfcb56f39bb4aed5b976ad76a90
Ankit Kumar <anshkrrr@gmail.com>
2022-07-25 21:34:18 +0530

    Update 005.bellman-ford(negative-weight-cycle).cc

--- GFG/Graph/005.bellman-ford(negative-weight-cycle).cc
+++ GFG/Graph/005.bellman-ford(negative-weight-cycle).cc
@@ -22,3 +22,39 @@ public:
 	}
 };
 Expected Time Complexity: O(E*V);
+
+
+
+
+/*  Function to implement Dijkstra
+    *   adj: vector of vectors which represents the graph
+    *   S: source vertex to start traversing graph with
+    *   V: number of vertices
+    */
+class Solution{
+public:
+    vector <int> bellman_ford(int V, vector<vector<int>> edges, int S) {
+        vector<int> dist(V,100000000);
+        dist[S] = 0;
+        
+        for(int i=0;i<V-1;i++){
+            for(auto edge: edges){
+	         if(dist[edge[0]]==1e8) continue;
+	         if(dist[edge[0]] + edge[2] < dist[edge[1]]){
+	             dist[edge[1]] = dist[edge[0]] + edge[2];
+	         }
+	    }
+        }
+        
+        for(auto edge: edges){
+            if(dist[edge[0]] == 1e8) continue;
+            if(dist[edge[0]] + edge[2] < dist[edge[1]]){
+                vector<int> temp(V,1e8);
+                return temp;
+            }
+        }
+    
+        return dist;
+    }
+};
+	
