d3b8d85a59b3f7e853c324484303eeae5bc393df
Ankit Kumar <anshkrrr@gmail.com>
2022-07-31 23:12:17 +0530

    Update #Maximum Index.cc

--- GFG/Array/#Maximum Index.cc
+++ GFG/Array/#Maximum Index.cc
@@ -1,4 +1,37 @@
 class Solution{
+public:  // TC - O(NlogN)
+    int maxIndexDiff(int arr[], int n) {
+        int rightMax[n];
+        rightMax[n-1] = arr[n-1];
+        for(int i=n-2;i>=0;i--)
+            rightMax[i] = max(rightMax[i+1],arr[i]);
+        
+        
+       vector<int> v;
+       int maxi = 0;
+       for(int i=0;i<n;i++){
+           int low = i+1; int high = n-1; int ans = -1;
+           while(low<=high){
+               int mid = (low+high)/2;
+               if(arr[i] <= rightMax[mid]){
+                   ans = mid;
+                    low = mid+1;
+               }else{
+                   high = mid-1;
+               }
+           }
+        //   v.push_back(ans);
+           maxi = max(maxi,ans - i);
+       }
+       return maxi;
+    }
+
+};
+
+
+
+// TC- O(N)
+class Solution{
 public:
     int maxIndexDiff(int arr[], int n) {
         int maxi = 0;
@@ -25,4 +58,4 @@ public:
 };
 
 
-O(N)
+
