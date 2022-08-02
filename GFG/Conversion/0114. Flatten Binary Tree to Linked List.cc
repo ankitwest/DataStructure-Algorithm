0214de3ce7126628fee8f357b8c80b4d5a1f07d2
Ankit Kumar <anshkrrr@gmail.com>
2022-08-03 02:20:06 +0530

    Update 0114. Flatten Binary Tree to Linked List.cc

--- GFG/Conversion/0114. Flatten Binary Tree to Linked List.cc
+++ GFG/Conversion/0114. Flatten Binary Tree to Linked List.cc
@@ -48,3 +48,28 @@ public:
 };
 
 
+// TC - O(N)  SC - O(N)
+class Solution4 {
+public:       
+    void flatten(TreeNode* root) {
+        if (!root) return;
+        stack<TreeNode*> st;
+        st.push(root);
+        
+        while (!st.empty()){
+            TreeNode *curr = st.top(); st.pop();
+            
+            if(curr->right)  
+                st.push(curr->right);
+            if(curr->left)   
+                st.push(curr->left);
+            
+            if(!st.empty()) 
+                 curr->right = st.top(); // make the right top
+             
+            curr->left = NULL;       // dont forget this!! 
+        }
+    }
+};
+
+
