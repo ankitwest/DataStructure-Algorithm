aa8bb87c188420eb4f95aa77344903409aa9d1de
Ankit Kumar <anshkrrr@gmail.com>
2022-08-03 02:02:32 +0530

    Create 0114. Flatten Binary Tree to Linked List.cc

---
+++ GFG/Conversion/0114. Flatten Binary Tree to Linked List.cc
@@ -0,0 +1,48 @@
+// Note:
+// if u want to reach at the last node use while(curr->next)
+
+
+// Morris Traversal 
+// TC -  O(N) SC - O(1) space
+// Iterative preorder
+class Solution {
+public:     
+    void flatten(TreeNode* root) {
+        // TreeNode* curr = root;
+        while(root)
+        {
+            if(root->left)  // if it exists
+            {
+                TreeNode* last = root->left;   //last guy of left subtree
+                while(last->right)
+                    last = last->right;
+                
+                last->right = root->right;
+                root->right = root->left;
+                root->left = nullptr;
+            }
+            root = root->right;
+        }
+    }
+};
+
+
+// recusion
+// reverse postorder
+// TC - O(N)  SC - O(N)
+class Solution2 {
+    TreeNode *prev = NULL;
+public:   
+     void flatten(TreeNode *root) {
+        if(!root) return;
+        
+        flatten(root->right);
+        flatten(root->left);
+        
+        root->right = prev;
+        root->left = NULL;
+        prev = root;
+    }
+};
+
+
