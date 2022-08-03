95c37e21590d4370fe6de1d3fa97ece339a78442
Ankit Kumar <anshkrrr@gmail.com>
2022-08-03 16:45:40 +0530

    Update Z:m-colouring-problem.cc

--- GFG/Graph/Z:m-colouring-problem.cc
+++ GFG/Graph/Z:m-colouring-problem.cc
@@ -1,27 +1,34 @@
 class Solution{
-  public:
-  bool safeColor(bool graph[101][101],int node,int c,vector<int> &color,int V){
-    for(int i=0;i<V;i++){
-        if(i==node) continue;
-        if(graph[i][node]==1 and color[i]==c) return false;
-    }
-    return true;
-}
-bool coloring(bool graph[101][101],int node,vector<int> &color,int V,int m){
-    if(node==V) return true;
-    
-    for(int k=0;k<m;k++){
-        if(safeColor(graph,node,k,color,V)){
-            color[node]=k;
-            if(coloring(graph,node+1,color,V,m)) return true;
-            color[node]=-1;
+vector<int> color; 
+int V; int M;
+
+    bool safeColor(int curr,int c,bool graph[101][101]){
+        for(int it=0;it<V;it++){
+            if(it==curr) continue;
+            if(graph[it][curr]==1 and color[it]==c) return false;
         }
+        return true;
     }
-    return false;
-}
-bool graphColoring(bool graph[101][101], int m, int V)
-{
-    vector<int> color(V,-1);
-    return coloring(graph,0,color,V,m);
-}
+    bool coloring(int node,bool graph[101][101]){
+        if(node==V) //[0,V-1]
+        return true; 
+        
+        for(int c=0;c<M;c++) //color loop
+        {  
+            if(safeColor(node,c,graph))
+            {
+                color[node] = c;
+                if(coloring(node+1,graph)) return true;
+                color[node] = -1;
+            }
+        }
+        return false;
+    }
+public:    
+    bool graphColoring(bool graph[101][101], int m, int n)
+    {
+        V = n; M = m;
+        color.resize(V,-1);
+        return coloring(0,graph);
+    }
 };
