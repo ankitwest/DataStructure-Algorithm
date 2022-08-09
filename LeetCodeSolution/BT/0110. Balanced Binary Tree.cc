9bb529db7107f9b69df5be3bd2c3dbcae55619fb
Ankit Kumar <anshkrrr@gmail.com>
2022-08-09 16:57:38 +0530

    Update 0110. Balanced Binary Tree.cc

--- LeetCodeSolution/BT/0110. Balanced Binary Tree.cc
+++ LeetCodeSolution/BT/0110. Balanced Binary Tree.cc
@@ -1,11 +1,6 @@
 
-class Solution {
-public:
-    bool ans = true;
-    bool isBalanced(TreeNode* root) {
-        int h = height(root);
-        return ans;
-    }
+class Solution1 {
+bool ans = true;
     int height(TreeNode* root){
         if(!root) return 0;
         
@@ -16,6 +11,39 @@ public:
             ans = false;
         }
         
+        return max(lh,rh) + 1;
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        int h = height(root);
+        return ans;
+    }
+
+};
+
+/*
+[1,2,2,3,3,null,null,4,4]
+[1,2,2,3,null,null,3,4,null,null,4]
+
+*/
+
+
+without global variable
+
+class Solution {
+    int height(TreeNode* root){
+        if(!root) return 0;
+        
+        int lh=height(root->left);
+        int rh=height(root->right);
+        
+        if(lh==-1 or rh==-1 or abs(lh-rh)>1) return -1;
+        
         return max(lh,rh)+1;
     }
+public:
+    bool isBalanced(TreeNode* root) {
+        int h = height(root);
+        return h == -1 ? 0 : 1;
+    } 
 };
