eb5d013b16244ad951f63a8f7f0ad7eae67cf05f
Ankit Kumar <anshkrrr@gmail.com>
2022-08-10 23:31:28 +0530

    Update and rename 0719. Find K-th Smallest Pair Distance.cc to #0719. Find K-th Smallest Pair Distance.cc

--- LeetCodeSolution/BS/#0719. Find K-th Smallest Pair Distance.cc
+++ LeetCodeSolution/BS/#0719. Find K-th Smallest Pair Distance.cc
@@ -1,12 +1,16 @@
-
 class Solution {
 public:
     int smallestDistancePair(vector<int>& nums, int k) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
-        int low = 0; int high = nums[n-1]-nums[0];
         
-        auto count = [&](int mid){
+        int low=INT_MAX;
+        for(int i=0;i<n-1;i++)
+            low = min(low,nums[i+1]-nums[i]);
+        
+        int high = nums[n-1]-nums[0];
+        
+        auto count2 = [&](int mid){
             int cnt = 0; int j=0;  // two pointer
             for(int i=0;i<n;i++){
                 while(j<n and nums[j]-nums[i]<=mid)j++;
@@ -14,7 +18,19 @@ public:
                 cnt += j-i;  // pairs
             }
             return cnt;
+        };  
+    
+        auto count = [&](int mid)->int{ //check
+            int cnt = 0; int j = 0;  
+
+            for(int i=0;i<n;i++){
+                int tar = nums[i]+mid;
+                auto it = upper_bound(nums.begin()+i,nums.end(),tar) - nums.begin();
+                cnt += it - (i+1);
+            }
+            return cnt;
         };
+        
         while(low<high){
             int mid = (low+high)>>1;
             if(count(mid)>=k)
